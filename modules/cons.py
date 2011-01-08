e7f79f5aca2dff46ef3c43a512078087aac55333
Giuseppe Penone <giuspen@gmail.com>
2011-01-08 19:56:56 +0200

    just updated version num for upcoming release, then updated the changelog

--- modules/cons.py
+++ modules/cons.py
@@ -22,7 +22,7 @@
 import os, sys
 
 
-VERSION = "0.19"
+VERSION = "0.19.1"
 APP_NAME = "cherrytree"
 NEWER_VERSION_URL = "http://www.giuspen.com/software/version_cherrytree"
 if sys.platform[0:3] == "win":
