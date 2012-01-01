46f6eb3a26e9a6320b5c315fcfe70e763f884976
Giuseppe Penone <Giuseppe Penone@localhost>
2012-01-01 18:29:45 +0200

    working on cherrytree to db full

--- modules/ctdb.py
+++ modules/ctdb.py
@@ -31,6 +31,21 @@ class CTDBHandler:
         """CherryTree DataBase boot"""
         self.dad = dad
     
+    def get_codebox_db_tuple(self, codebox_element, node_id):
+        """From codebox element to db tuple"""
+        offset = codebox_element[0]
+        codebox = codebox_element[1]
+        justification = codebox_element[2]
+        txt = codebox['fill_text']
+        syntax = codebox['syntax_highlighting']
+        width = codebox['frame_width']
+        height = codebox['frame_height']
+        is_width_pix = codebox['width_in_pixels']
+        do_highl_bra = codebox['highlight_brackets']
+        do_show_linenum = codebox['show_line_numbers']
+        return (node_id, offset, justification, txt, syntax,
+                width, height, is_width_pix, do_highl_bra, do_show_linenum)
+    
     def new_db(self, dbpath):
         """Create a new DataBase"""
         db = sqlite3.connect(dbpath)
@@ -52,7 +67,7 @@ class CTDBHandler:
             db.execute('INSERT INTO bookmark VALUES(?)', bookmark_tuple)
         db.commit()
     
-    def write_db_node(self, db, tree_iter, level):
+    def write_db_node(self, db, tree_iter, level, sequence):
         """Write a node in DB"""
         node_id = self.dad.treestore[tree_iter][3]
         name = self.dad.treestore[tree_iter][1]
@@ -84,33 +99,52 @@ class CTDBHandler:
                 self.dad.xml_handler.rich_txt_serialize(dom_iter, start_iter, curr_iter, self.curr_attributes, dom=self.dom)
                 if curr_iter.compare(end_iter) == 0: break
                 else:
-                    self.rich_text_attributes_update(curr_iter, self.curr_attributes)
+                    self.dad.xml_handler.rich_text_attributes_update(curr_iter, self.curr_attributes)
                     offset_old = curr_iter.get_offset()
                     start_iter.set_offset(offset_old)
                     tag_found = curr_iter.forward_to_tag_toggle(None)
                     if curr_iter.get_offset() == offset_old: break
             else:  self.dad.xml_handler.rich_txt_serialize(dom_iter, start_iter, curr_iter, self.curr_attributes, dom=self.dom)
-            # in case of writing to disk it's time to serialize the info about the images
-            if to_disk == True:
-                pixbuf_table_codebox_vector = self.dad.state_machine.get_embedded_pixbufs_tables_codeboxes(curr_buffer)
-                # pixbuf_table_codebox_vector is [ [ "pixbuf"/"table"/"codebox", [offset, pixbuf, alignment] ],... ]
-                for element in pixbuf_table_codebox_vector:
-                    if element[0] == "pixbuf": self.pixbuf_element_to_xml(element[1], dom_iter, self.dom)
-                    elif element[0] == "table": self.table_element_to_xml(element[1], dom_iter)
-                    elif element[0] == "codebox": self.codebox_element_to_xml(element[1], dom_iter)
+            # time to retrieve the objects
+            pixbuf_table_codebox_vector = self.dad.state_machine.get_embedded_pixbufs_tables_codeboxes(curr_buffer)
+            # pixbuf_table_codebox_vector is [ [ "pixbuf"/"table"/"codebox", [offset, pixbuf, alignment] ],... ]
+            codeboxes_tuples = []
+            tables_tuples = []
+            images_tuples = []
+            for element in pixbuf_table_codebox_vector:
+                if element[0] == "pixbuf":
+                    self.pixbuf_element_to_xml(element[1], dom_iter, self.dom)
+                elif element[0] == "table":
+                    self.table_element_to_xml(element[1], dom_iter)
+                elif element[0] == "codebox":
+                    self.codebox_element_to_xml(element[1], dom_iter)
+            if codeboxes_tuples:
+                has_codebox = 1
+                for codebox_tuple in codeboxes_tuples:
+                    db.execute('INSERT INTO codebox VALUES(?,?,?,?,?,?,?,?,?,?)', codebox_tuple)
+            if tables_tuples:
+                has_table = 1
+                for table_tuple in tables_tuples:
+                    db.execute('INSERT INTO table VALUES(?,?,?,?,?,?)', table_tuple)
+            if images_tuples:
+                has_image = 1
+                for image_tuple in images_tuples:
+                    db.execute('INSERT INTO image VALUES(?,?,?,?)', image_tuple)
             # retrieve xml text
             txt = self.dom.toxml()
         else: txt = start_iter.get_text(end_iter)
         node_tuple = (node_id, name, txt, syntax, tags,
                       is_ro, is_richtxt, has_codebox, has_table, has_image,
-                      has_children, level)
-        db.execute('INSERT INTO node VALUES(?,?,?,?,?,?,?,?,?,?,?,?)', bookmark_tuple)
+                      has_children, level, sequence)
+        db.execute('INSERT INTO node VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)', bookmark_tuple)
         db.commit()
     
     def write_db_full(self, db):
         """Write the whole DB"""
         tree_iter = self.dad.treestore.get_iter_first()
+        sequence = 0
         while tree_iter != None:
-            self.write_db_node(db, tree_iter, 0)
+            sequence += 1
+            self.write_db_node(db, tree_iter, 1, sequence)
             tree_iter = self.dad.treestore.iter_next(tree_iter)
         self.write_db_bookmarks(db)
