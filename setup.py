255ac8bdddd80376e1ad265aa64c6f01b5dfb032
Giuseppe Penone <giuspen@gmail.com>
2013-07-29 21:22:33 +0200

    fixing py2exe for enchant

--- setup.py
+++ setup.py
@@ -168,11 +168,8 @@ if "py2exe" in sys.argv:
        options={"py2exe": {
                    "includes": "pango,cairo,pangocairo,atk,gobject,gtk,gtksourceview2,gio,enchant",
                    "dll_excludes": [
-                                 "iconv.dll","intl.dll","libatk-1.0-0.dll",
-                                 "libgdk_pixbuf-2.0-0.dll","libgdk-win32-2.0-0.dll",
-                                 "libglib-2.0-0.dll","libgmodule-2.0-0.dll",
-                                 "libgobject-2.0-0.dll","libgthread-2.0-0.dll",
-                                 "libgtk-win32-2.0-0.dll","libpango-1.0-0.dll",
+                                 "libgdk-win32-2.0-0.dll",
+                                 "libgtk-win32-2.0-0.dll",
                                  "libpangowin32-1.0-0.dll"],
                           }
                 },
