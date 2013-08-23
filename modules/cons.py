4c3cba92bc5081edffdda8f8de2f368073687428
Giuseppe Penone <giuspen@gmail.com>
2013-08-23 21:37:54 +0200

    GPLV2+ to GPLV3+

--- modules/cons.py
+++ modules/cons.py
@@ -6,7 +6,7 @@
 #
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation; either version 2 of the License, or
+#       the Free Software Foundation; either version 3 of the License, or
 #       (at your option) any later version.
 #
 #       This program is distributed in the hope that it will be useful,
@@ -22,7 +22,7 @@
 import os, sys
 
 
-VERSION = "0.30.0"
+VERSION = "0.30.1"
 APP_NAME = "cherrytree"
 NEWER_VERSION_URL = "http://www.giuspen.com/software/version_cherrytree"
 if sys.platform[0:3] == "win":
