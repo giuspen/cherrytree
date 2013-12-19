dcaa18a02a5f2f62cae3378c92c700967e1d2f84
Giuseppe Penone <Giuseppe Penone@localhost>
2013-12-19 11:19:19 +0100

    updated languages files after preferences dialog changes

--- locale/i18n_update_pot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd ..
-
-intltool-extract --type=gettext/glade glade/cherrytree.glade
-
-xgettext --language=Python --from-code=utf-8 --keyword=_ --keyword=N_ --output=locale/cherrytree.pot modules/support.py modules/findreplace.py modules/core.py modules/tablez.py modules/codeboxes.py modules/cons.py modules/imports.py modules/pgsc_spellcheck.py glade/cherrytree.glade.h
