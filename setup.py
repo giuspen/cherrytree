9e2fb82b502c39584b4b10c99bd8c1dcbb31411b
Giuseppe Penone <giuspen@gmail.com>
2014-05-31 00:10:47 +0200

    added appdata file, updated changelog, manual-install, version num

--- setup.py
+++ setup.py
@@ -194,6 +194,7 @@ else:
                       ("share/mime/packages", ["linux/cherrytree.xml"]),
                       ("share/mime-info", ["linux/cherrytree.mime", "linux/cherrytree.keys"]),
                       ("share/application-registry", ["linux/cherrytree.applications"]),
+                      ("share/appdata", ["linux/cherrytree.appdata.xml"]),
                       ("share/man/man1", ["linux/cherrytree.1.gz"])
                    ],
        cmdclass={
