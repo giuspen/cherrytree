fb28494c0335d52228003e47c62a29c1b29b9441
Giuseppe Penone <giuspen@gmail.com>
2014-08-16 19:42:28 +0200

    preparing 0.34.1 issue

--- modules/cons.py
+++ modules/cons.py
@@ -22,7 +22,7 @@
 import os, sys
 
 
-VERSION = "0.34.0"
+VERSION = "0.34.1"
 APP_NAME = "cherrytree"
 NEWER_VERSION_URL = "http://www.giuspen.com/software/version_cherrytree"
 if sys.platform.startswith("win"):
