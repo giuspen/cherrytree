a75d5ad98fcaeb03eb479c970f0917525647cb36
Giuseppe Penone <giuspen@gmail.com>
2015-05-25 14:36:56 +0100

    0.35.8 changelog

--- modules/cons.py
+++ modules/cons.py
@@ -22,7 +22,7 @@
 import os, sys
 
 
-VERSION = "0.35.7"
+VERSION = "0.35.8"
 APP_NAME = "cherrytree"
 NEWER_VERSION_URL = "http://www.giuspen.com/software/version_cherrytree"
 if sys.platform.startswith("win"):
