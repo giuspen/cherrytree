2715f970295d39449f5cee95e93e2b32a791e016
Giuseppe Penone <giuspen@gmail.com>
2016-01-24 21:16:57 +0000

    prepared changelog for upcoming 0.36.4

--- modules/cons.py
+++ modules/cons.py
@@ -22,7 +22,7 @@
 import os, sys
 
 
-VERSION = "0.36.3"
+VERSION = "0.36.4"
 APP_NAME = "cherrytree"
 NEWER_VERSION_URL = "http://www.giuspen.com/software/version_cherrytree"
 if sys.platform.startswith("win"):
