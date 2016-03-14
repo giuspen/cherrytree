cb77a1d1fada018c6fccb9d06c01bd0be6965f90
Giuseppe Penone <giuspen@gmail.com>
2016-03-14 21:47:04 +0000

    0.36.8 chengelog

--- modules/cons.py
+++ modules/cons.py
@@ -22,7 +22,7 @@
 import os, sys
 
 
-VERSION = "0.36.7"
+VERSION = "0.36.8"
 APP_NAME = "cherrytree"
 NEWER_VERSION_URL = "http://www.giuspen.com/software/version_cherrytree"
 if sys.platform.startswith("win"):
