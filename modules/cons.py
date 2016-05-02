2b13308992995e52c1ac3e6e87b25b96f55de96a
Giuseppe Penone <giuspen@gmail.com>
2016-05-02 18:33:47 +0100

    added leaf to the user selectable node icons

--- modules/cons.py
+++ modules/cons.py
@@ -249,9 +249,9 @@ STOCKS_N_FILES = [
 'add', 'cancel', 'done', 'java', 'mail', 'notes', 'python', 'remove', 'star',
 'terminal', 'terminal-red', 'warning', 'home', 'code', 'html',
 'circle-green', 'circle-grey', 'circle-red', 'circle-yellow',
-'locked', 'unlocked', 'people', 'urgent', 'folder',
+'locked', 'unlocked', 'people', 'urgent', 'folder', 'leaf',
 ]
-NODES_STOCKS_KEYS = [1,2,3,4,5,6,7,8,9,40,41,42,10,43,11,12,13,14,15,16,44,18,19,20,39,38,21,22,23,24,17,25,26,27,28,29,30,31,32,33,34,35,36,37]
+NODES_STOCKS_KEYS = [1,2,3,4,5,6,7,8,9,40,41,42,10,43,11,12,13,14,15,16,44,18,19,20,39,38,21,22,23,24,17,25,26,45,27,28,29,30,31,32,33,34,35,36,37]
 NODES_STOCKS = {
  1: 'circle-green',
  2: 'circle-yellow',
@@ -297,6 +297,7 @@ NODES_STOCKS = {
 42: 'people',
 43: 'urgent',
 44: 'folder',
+45: 'leaf',
 }
 NODES_ICONS = {
 0:'cherry_red', 1:'cherry_blue', 2:'cherry_orange', 3:'cherry_cyan',
