0d4f37112a9b7eb91c50356527aa7e4720fd9d76
Giuseppe Penone <giuspen@gmail.com>
2016-05-13 21:49:26 +0100

    added c and c++ to the user selectable node icons

--- modules/cons.py
+++ modules/cons.py
@@ -249,9 +249,9 @@ STOCKS_N_FILES = [
 'add', 'cancel', 'done', 'java', 'mail', 'notes', 'python', 'remove', 'star',
 'terminal', 'terminal-red', 'warning', 'home', 'code', 'html',
 'circle-green', 'circle-grey', 'circle-red', 'circle-yellow',
-'locked', 'unlocked', 'people', 'urgent', 'folder', 'leaf', 'xml'
+'locked', 'unlocked', 'people', 'urgent', 'folder', 'leaf', 'xml', 'c', 'cpp',
 ]
-NODES_STOCKS_KEYS = [1,2,3,4,5,6,7,8,9,40,41,42,10,43,11,12,13,14,15,16,44,18,19,20,39,38,21,22,23,24,46,17,25,26,45,27,28,29,30,31,32,33,34,35,36,37]
+NODES_STOCKS_KEYS = [1,2,3,4,5,6,7,8,9,40,41,42,10,43,11,12,13,14,15,16,44,18,19,20,39,38,21,22,23,24,46,47,48,17,25,26,45,27,28,29,30,31,32,33,34,35,36,37]
 NODES_STOCKS = {
  1: 'circle-green',
  2: 'circle-yellow',
@@ -299,6 +299,8 @@ NODES_STOCKS = {
 44: 'folder',
 45: 'leaf',
 46: 'xml',
+47: 'c',
+48: 'cpp',
 }
 NODES_ICONS = {
 0:'cherry_red', 1:'cherry_blue', 2:'cherry_orange', 3:'cherry_cyan',
@@ -311,6 +313,8 @@ CODE_ICONS = {
 "java": 'java',
 "html": 'html',
 "xml": 'xml',
+"c": 'c',
+"cpp": 'cpp',
 }
 NODE_ICON_CODE_ID = 38
 NODE_ICON_BULLET_ID = 25
