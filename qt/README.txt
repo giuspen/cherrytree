ef0596b7412e68b5c754e7ace6b562da7d721f3b
Giuseppe Penone <giuspen@gmail.com>
2017-06-08 00:21:52 +0100

    qt - qscintilla c++

--- qt/README.txt
+++ qt/README.txt
@@ -4,12 +4,19 @@ https://www.python.org/downloads/
 https://www.riverbankcomputing.com/software/pyqt/download5
 https://www.riverbankcomputing.com/software/qscintilla/download
 
-
 Dependencies for Linux
 sudo pip3 install PyQt5
 sudo pip3 install QScintilla
 
-
 Dependencies for Windows
 pip install PyQt5
 pip install QScintilla
+
+
+...or Qt5 and QScintilla
+
+https://www.qt.io/download-open-source/
+https://www.riverbankcomputing.com/software/qscintilla/download
+
+Dependencies for Linux Debian based
+sudo apt install qt5-default libqt5declarative5 libqt5scintilla2-dev
--- qt/sandbox/qscintilla.cpp
+++ qt/sandbox/qscintilla.cpp
@@ -10,14 +10,19 @@ class MyQScintilla : public QsciScintilla
 {
 public:
     MyQScintilla(QsciScintilla *parent = 0);
+    ~MyQScintilla();
     void setContent(const QString content_text);
+
+protected:
+    QsciLexerCPP *mp_lexerCpp;
+    void onModificationChanged(bool m);
 };
 
 
 MyQScintilla::MyQScintilla(QsciScintilla *parent) : QsciScintilla(parent)
 {
-    QsciLexerCPP *p_lexerCpp = new QsciLexerCPP();
-    setLexer(p_lexerCpp);
+    mp_lexerCpp = new QsciLexerCPP();
+    setLexer(mp_lexerCpp);
     setWhitespaceVisibility(WsVisible);
     setMarginLineNumbers(1, true);
     setMarginWidth(1, 25);
@@ -32,11 +37,23 @@ MyQScintilla::MyQScintilla(QsciScintilla *parent) : QsciScintilla(parent)
     setCaretLineBackgroundColor(QColor("#e5e5e5"));
 }
 
+MyQScintilla::~MyQScintilla()
+{
+    delete mp_lexerCpp;
+}
+
 void MyQScintilla::setContent(const QString content_text)
 {
     setText(content_text);
     setModified(false);
+    connect(this, &MyQScintilla::modificationChanged, this, &MyQScintilla::onModificationChanged);
 }
+
+void MyQScintilla::onModificationChanged(bool m)
+{
+    qInfo() << "modified " << m;
+}
+
 
 int main(int argc, char *argv[])
 {
