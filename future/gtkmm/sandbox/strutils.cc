ce2d7270095b7dd74a4176e67314c17a67a9f3cd
Giuseppe Penone <giuspen@gmail.com>
2017-07-08 19:05:44 +0100

    gtkmm3 WIP

--- future/gtkmm/sandbox/strutils.cc
+++ future/gtkmm/sandbox/strutils.cc
@@ -27,10 +27,10 @@
 #include <glibmm.h>
 
 
-std::list<Glib::ustring> ustring_split(const gchar *in_str, const gchar *delimiter, gint max_tokens=-1)
+std::list<Glib::ustring> gstring_split2ustring(const gchar *in_str, const gchar *delimiter, gint max_tokens=-1)
 {
     std::list<Glib::ustring> ret_list;
-    gchar** array_of_strings = g_strsplit(in_str, delimiter, max_tokens);
+    gchar **array_of_strings = g_strsplit(in_str, delimiter, max_tokens);
     for(gchar** ptr = array_of_strings; *ptr; ptr++)
     {
         ret_list.push_back(*ptr);
@@ -40,7 +40,21 @@ std::list<Glib::ustring> ustring_split(const gchar *in_str, const gchar *delimit
 }
 
 
-Glib::ustring ustring_join(std::list<Glib::ustring>& in_str_list, const gchar *delimiter)
+std::list<gint64> gstring_split2int64(const gchar *in_str, const gchar *delimiter, gint max_tokens=-1)
+{
+    std::list<gint64> ret_list;
+    gchar **array_of_strings = g_strsplit(in_str, delimiter, max_tokens);
+    for(gchar** ptr = array_of_strings; *ptr; ptr++)
+    {
+        gint64 curr_int = g_ascii_strtoll(*ptr, NULL, 10);
+        ret_list.push_back(curr_int);
+    }
+    g_strfreev(array_of_strings);
+    return ret_list;
+}
+
+
+Glib::ustring ustring_join4ustring(std::list<Glib::ustring>& in_str_list, const gchar *delimiter)
 {
     Glib::ustring ret_str;
     bool first_iteration = true;
@@ -54,6 +68,29 @@ Glib::ustring ustring_join(std::list<Glib::ustring>& in_str_list, const gchar *d
 }
 
 
+Glib::ustring ustring_from_int64(gint64 in_int64)
+{
+    gchar *p_str = g_strdup_printf("%ld", in_int64);
+    Glib::ustring ret_ustr(p_str);
+    g_free(p_str);
+    return ret_ustr;
+}
+
+
+Glib::ustring ustring_join4int64(std::list<gint64>& in_int64_list, const gchar *delimiter)
+{
+    Glib::ustring ret_str;
+    bool first_iteration = true;
+    for(gint64 element : in_int64_list)
+    {
+        if(!first_iteration) ret_str += delimiter;
+        else first_iteration = false;
+        ret_str += ustring_from_int64(element);
+    }
+    return ret_str;
+}
+
+
 int main(int argc, char *argv[])
 {
     std::locale::global(std::locale("")); // Set the global C++ locale to the user-specified locale
@@ -62,13 +99,27 @@ int main(int argc, char *argv[])
     const gchar str_delimiter[] = ":";
 
     std::cout << str_orig << std::endl;
-    std::list<Glib::ustring> splitted_list = ustring_split(str_orig, str_delimiter);
+    std::list<Glib::ustring> splitted_list = gstring_split2ustring(str_orig, str_delimiter);
     for(Glib::ustring str_elem : splitted_list)
     {
         std::cout << str_elem << std::endl;
     }
-    Glib::ustring rejoined = ustring_join(splitted_list, str_delimiter);
+    Glib::ustring rejoined = ustring_join4ustring(splitted_list, str_delimiter);
     std::cout << rejoined << std::endl;
 
     assert(rejoined == str_orig);
+
+    const gchar str_int64_orig[] = "-1,1,0,1000";
+    const gchar str_delimiter_int64[] = ",";
+
+    std::cout << str_int64_orig << std::endl;
+    std::list<gint64> splitted_list_int64 = gstring_split2int64(str_int64_orig, str_delimiter_int64);
+    for(gint64 int_elem : splitted_list_int64)
+    {
+        std::cout << int_elem << std::endl;
+    }
+    Glib::ustring rejoined_int64 = ustring_join4int64(splitted_list_int64, str_delimiter_int64);
+    std::cout << rejoined_int64 << std::endl;
+
+    assert(rejoined_int64 == str_int64_orig);
 }
