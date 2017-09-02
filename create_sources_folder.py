0326330ffe618dd00d9aacf30af2003fbbd83f84
Giuseppe Penone <giuspen@gmail.com>
2017-09-02 09:12:57 +0100

    fixed script create_sources_folder.py

--- create_sources_folder.py
+++ create_sources_folder.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 
-import os, sys, shutil, glob, __builtin__
+import os, sys, shutil, glob, subprocess, __builtin__
+
+BLACKLIST = [".git", ".gitignore", "sandbox"]
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 MODULES_DIR = os.path.join(SCRIPT_DIR, "modules")
@@ -10,18 +12,14 @@ sys.path.insert(0, MODULES_DIR)
 __builtin__.SHARE_PATH = SCRIPT_DIR
 import cons
 
-BLACKLIST = [".git", ".gitignore", "future", "sandbox"]
-
 DEST_DIR = os.path.join(os.path.dirname(SCRIPT_DIR), "cherrytree-"+cons.VERSION)
 if len(sys.argv) > 1:
-    DEST_DIR += "+r" + sys.argv[1]
-#print DEST_DIR
+    DEST_DIR += "+" + sys.argv[1]
 
 if not os.path.isdir(DEST_DIR):
     os.mkdir(DEST_DIR)
 
-for pycfilepath in glob.glob(os.path.join(MODULES_DIR, "*.pyc")):
-    os.remove(pycfilepath)
+subprocess.call(["git", "clean", "-dfx"])
 
 for element in os.listdir(SCRIPT_DIR):
     if not element in BLACKLIST:
@@ -31,3 +29,5 @@ for element in os.listdir(SCRIPT_DIR):
             shutil.copytree(src_abspath, dst_abspath)
         else:
             shutil.copy2(src_abspath, dst_abspath)
+
+print "-->", DEST_DIR
