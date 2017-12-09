54a5f8dbe25b38caf926c9333159f572b8bee523
Giuseppe Penone <giuspen@gmail.com>
2017-12-09 16:53:28 +0000

    added 7zr 16.02 building with autotools

--- sandbox/gtkmm/7zr/autogen.sh
+++ sandbox/gtkmm/7zr/autogen.sh
@@ -2,6 +2,10 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
+if [ ! -d "$srcdir"/po ]; then
+    mkdir "$srcdir"/po
+fi
+
 autoreconf --force --install --verbose --warnings=all "$srcdir"
 echo "Running intltoolize --copy --force --automake"
 intltoolize --copy --force --automake
