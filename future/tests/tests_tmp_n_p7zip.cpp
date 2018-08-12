cd830308b5b5caea762d806ca5af69a5348e1b9a
Giuseppe Penone <giuspen@gmail.com>
2018-08-12 20:33:14 +0300

    gtkmm - unit tests for p7za

--- future/tests/tests_tmp_n_p7zip.cpp
+++ future/tests/tests_tmp_n_p7zip.cpp
@@ -20,6 +20,9 @@
  */
 
 #include "ct_app.h"
+#include "p7za_iface.h"
+#include <glib/gstdio.h>
+#include <libxml++/libxml++.h>
 #include "CppUTest/CommandLineTestRunner.h"
 
 
@@ -33,4 +36,37 @@ TEST(TmpP7zipGroup, CTTmp_misc)
     CHECK(g_file_test(pCTmp->getRootDirpath(), G_FILE_TEST_IS_DIR));
     delete pCTmp;
     CHECK(!g_file_test(pCTmp->getRootDirpath(), G_FILE_TEST_IS_DIR));
+}
+
+TEST(TmpP7zipGroup, P7zaIfaceMisc)
+{
+    CTTmp ctTmp;
+    const Glib::ustring ctzInputPath{Glib::build_filename(_UNITTEST_DATA_DIR, "7zr.ctz")};
+    const Glib::ustring ctdTmpPath{Glib::build_filename(ctTmp.getRootDirpath(), "7zr.ctd")};
+    const Glib::ustring ctzTmpPath{Glib::build_filename(ctTmp.getRootDirpath(), "7zr2.ctz")};
+    const gchar testPassword[]{"7zr"};
+    for (auto filepath : std::list<Glib::ustring>{ctdTmpPath, ctzTmpPath})
+    {
+        if (Glib::file_test(filepath, Glib::FILE_TEST_EXISTS))
+        {
+            g_remove(filepath.c_str());
+        }
+    }
+    p7za_extract(ctzInputPath.c_str(), ctTmp.getRootDirpath(), testPassword);
+    CHECK(Glib::file_test(ctdTmpPath, Glib::FILE_TEST_EXISTS));
+    std::string xml_txt = Glib::file_get_contents(ctdTmpPath);
+    xmlpp::DomParser dom_parser;
+    dom_parser.parse_memory(xml_txt);
+    xmlpp::Document* p_document = dom_parser.get_document();
+    xmlpp::Element* p_element = p_document->get_root_node();
+    CHECK(0 == p_element->get_name().compare("cherrytree"));
+    CHECK(0 == static_cast<xmlpp::Element*>(p_element->find("node")[0])->get_attribute_value("name").compare("NodeName"));
+    CHECK(0 == static_cast<xmlpp::Element*>(p_element->find("node/rich_text")[0])->get_child_text()->get_content().compare("NodeContent"));
+    p7za_archive(ctdTmpPath.c_str(), ctzTmpPath.c_str(), testPassword);
+    CHECK(Glib::file_test(ctzTmpPath, Glib::FILE_TEST_EXISTS));
+    g_remove(ctdTmpPath.c_str());
+    p7za_extract(ctzTmpPath.c_str(), ctTmp.getRootDirpath(), testPassword);
+    CHECK(Glib::file_test(ctdTmpPath, Glib::FILE_TEST_EXISTS));
+    std::string xml_txt_bis = Glib::file_get_contents(ctdTmpPath);
+    CHECK(0 == xml_txt.compare(xml_txt_bis));
 }
