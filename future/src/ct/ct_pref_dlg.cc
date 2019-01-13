2367115024bb1cf55a73d2332dd2c164c88ff77f
Evgenii Guryanov <whats.eternal@gmail.com>
2019-01-13 21:28:08 +0300

    add pref tab: links

--- future/src/ct/ct_pref_dlg.cc
+++ future/src/ct/ct_pref_dlg.cc
@@ -671,8 +671,116 @@ Gtk::Widget* CtPrefDlg::build_tab_fonts()
 
 Gtk::Widget* CtPrefDlg::build_tab_links()
 {
+    CtConfig* config = CtApp::P_ctCfg;
+
+    Gtk::VBox* vbox_links_actions = Gtk::manage(new Gtk::VBox());
+    Gtk::CheckButton* checkbutton_custom_weblink_cmd = Gtk::manage(new Gtk::CheckButton(_("Enable Custom Web Link Clicked Action")));
+    Gtk::Entry* entry_custom_weblink_cmd = Gtk::manage(new Gtk::Entry());
+    Gtk::CheckButton* checkbutton_custom_filelink_cmd = Gtk::manage(new Gtk::CheckButton(_("Enable Custom File Link Clicked Action")));
+    Gtk::Entry* entry_custom_filelink_cmd = Gtk::manage(new Gtk::Entry());
+    Gtk::CheckButton* checkbutton_custom_folderlink_cmd = Gtk::manage(new Gtk::CheckButton(_("Enable Custom Folder Link Clicked Action")));
+    Gtk::Entry* entry_custom_folderlink_cmd = Gtk::manage(new Gtk::Entry());
+    vbox_links_actions->pack_start(*checkbutton_custom_weblink_cmd, false, false);
+    vbox_links_actions->pack_start(*entry_custom_weblink_cmd, false, false);
+    vbox_links_actions->pack_start(*checkbutton_custom_filelink_cmd, false, false);
+    vbox_links_actions->pack_start(*entry_custom_filelink_cmd, false, false);
+    vbox_links_actions->pack_start(*checkbutton_custom_folderlink_cmd, false, false);
+    vbox_links_actions->pack_start(*entry_custom_folderlink_cmd, false, false);
+
+    Gtk::Frame* frame_links_actions = Gtk::manage(new Gtk::Frame(std::string("<b>")+_("Custom Actions")+"</b>"));
+    ((Gtk::Label*)frame_links_actions->get_label_widget())->set_use_markup(true);
+    frame_links_actions->set_shadow_type(Gtk::SHADOW_NONE);
+    Gtk::Alignment* align_links_actions = Gtk::manage(new Gtk::Alignment());
+    align_links_actions->set_padding(3, 6, 6, 6);
+    align_links_actions->add(*vbox_links_actions);
+    frame_links_actions->add(*align_links_actions);
+
+    checkbutton_custom_weblink_cmd->set_active(config->weblinkCustomOn);
+    entry_custom_weblink_cmd->set_sensitive(config->weblinkCustomOn);
+    entry_custom_weblink_cmd->set_text(config->weblinkCustomAct);
+    checkbutton_custom_filelink_cmd->set_active(config->filelinkCustomOn);
+    entry_custom_filelink_cmd->set_sensitive(config->filelinkCustomOn);
+    entry_custom_filelink_cmd->set_text(config->filelinkCustomAct);
+    checkbutton_custom_folderlink_cmd->set_active(config->folderlinkCustomOn);
+    entry_custom_folderlink_cmd->set_sensitive(config->folderlinkCustomOn);
+    entry_custom_folderlink_cmd->set_text(config->folderlinkCustomAct);
+
+    Gtk::Table* table_links_colors = Gtk::manage(new Gtk::Table(2, 2));
+    table_links_colors->set_row_spacings(2);
+    table_links_colors->set_col_spacings(4);
+    table_links_colors->set_homogeneous(true);
+
+    Gtk::HBox* hbox_col_link_webs = Gtk::manage(new Gtk::HBox());
+    hbox_col_link_webs->set_spacing(4);
+    Gtk::Label* label_col_link_webs = Gtk::manage(new Gtk::Label(_("To WebSite")));
+    Gtk::ColorButton* colorbutton_col_link_webs = Gtk::manage(new Gtk::ColorButton(Gdk::RGBA(config->colLinkWebs)));
+    hbox_col_link_webs->pack_start(*label_col_link_webs, false, false);
+    hbox_col_link_webs->pack_start(*colorbutton_col_link_webs, false, false);
+
+    Gtk::HBox* hbox_col_link_node = Gtk::manage(new Gtk::HBox());
+    hbox_col_link_node->set_spacing(4);
+    Gtk::Label* label_col_link_node = Gtk::manage(new Gtk::Label(_("To Node")));
+    Gtk::ColorButton* colorbutton_col_link_node = Gtk::manage(new Gtk::ColorButton(Gdk::RGBA(config->colLinkNode)));
+    hbox_col_link_node->pack_start(*label_col_link_node, false, false);
+    hbox_col_link_node->pack_start(*colorbutton_col_link_node, false, false);
+
+    Gtk::HBox* hbox_col_link_file = Gtk::manage(new Gtk::HBox());
+    hbox_col_link_file->set_spacing(4);
+    Gtk::Label* label_col_link_file = Gtk::manage(new Gtk::Label(_("To File")));
+    Gtk::ColorButton* colorbutton_col_link_file = Gtk::manage(new Gtk::ColorButton(Gdk::RGBA(config->colLinkFile)));
+    hbox_col_link_file->pack_start(*label_col_link_file, false, false);
+    hbox_col_link_file->pack_start(*colorbutton_col_link_file, false, false);
+
+    Gtk::HBox* hbox_col_link_fold = Gtk::manage(new Gtk::HBox());
+    hbox_col_link_fold->set_spacing(4);
+    Gtk::Label* label_col_link_fold = Gtk::manage(new Gtk::Label(_("To Folder")));
+    Gtk::ColorButton* colorbutton_col_link_fold = Gtk::manage(new Gtk::ColorButton(Gdk::RGBA(config->colLinkFold)));
+    hbox_col_link_fold->pack_start(*label_col_link_fold, false, false);
+    hbox_col_link_fold->pack_start(*colorbutton_col_link_fold, false, false);
+
+    table_links_colors->attach(*hbox_col_link_webs, 0, 1, 0, 1);
+    table_links_colors->attach(*hbox_col_link_node, 0, 1, 1, 2);
+    table_links_colors->attach(*hbox_col_link_file, 1, 2, 0, 1);
+    table_links_colors->attach(*hbox_col_link_fold, 1, 2, 1, 2);
+
+    Gtk::Frame* frame_links_colors = Gtk::manage(new Gtk::Frame(std::string("<b>")+_("Colors")+"</b>"));
+    ((Gtk::Label*)frame_links_colors->get_label_widget())->set_use_markup(true);
+    frame_links_colors->set_shadow_type(Gtk::SHADOW_NONE);
+    Gtk::Alignment* align_links_colors = Gtk::manage(new Gtk::Alignment());
+    align_links_colors->set_padding(3, 6, 6, 6);
+    align_links_colors->add(*table_links_colors);
+    frame_links_colors->add(*align_links_colors);
+
+    Gtk::VBox* vbox_links_misc = Gtk::manage(new Gtk::VBox());
+    Gtk::CheckButton* checkbutton_links_underline = Gtk::manage(new Gtk::CheckButton(_("Underline Links")));
+    checkbutton_links_underline->set_active(config->linksUnderline);
+    Gtk::CheckButton* checkbutton_links_relative = Gtk::manage(new Gtk::CheckButton(_("Use Relative Paths for Files And Folders")));
+    checkbutton_links_relative->set_active(config->linksRelative);
+    Gtk::HBox* hbox_anchor_size = Gtk::manage(new Gtk::HBox());
+    hbox_anchor_size->set_spacing(4);
+    Gtk::Label* label_anchor_size = Gtk::manage(new Gtk::Label(_("Anchor Size")));
+    Glib::RefPtr<Gtk::Adjustment> adj_anchor_size = Gtk::Adjustment::create(config->anchorSize, 1, 1000, 1);
+    Gtk::SpinButton* spinbutton_anchor_size = Gtk::manage(new Gtk::SpinButton(adj_anchor_size));
+    spinbutton_anchor_size->set_value(config->anchorSize);
+    hbox_anchor_size->pack_start(*label_anchor_size, false, false);
+    hbox_anchor_size->pack_start(*spinbutton_anchor_size, false, false);
+    vbox_links_misc->pack_start(*checkbutton_links_underline, false, false);
+    vbox_links_misc->pack_start(*checkbutton_links_relative, false, false);
+    vbox_links_misc->pack_start(*hbox_anchor_size, false, false);
+
+    Gtk::Frame* frame_links_misc = Gtk::manage(new Gtk::Frame(std::string("<b>")+_("Miscellaneous")+"</b>"));
+    ((Gtk::Label*)frame_links_misc->get_label_widget())->set_use_markup(true);
+    frame_links_misc->set_shadow_type(Gtk::SHADOW_NONE);
+    Gtk::Alignment* align_links_misc = Gtk::manage(new Gtk::Alignment());
+    align_links_misc->set_padding(3, 6, 6, 6);
+    align_links_misc->add(*vbox_links_misc);
+    frame_links_misc->add(*align_links_misc);
+
     Gtk::VBox* pMainBox = Gtk::manage(new Gtk::VBox());
     pMainBox->set_spacing(3);
+    pMainBox->pack_start(*frame_links_actions, false, false);
+    pMainBox->pack_start(*frame_links_colors, false, false);
+    pMainBox->pack_start(*frame_links_misc, false, false);
     return pMainBox;
 }
 
