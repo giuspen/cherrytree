57c078ee7cc1c78c04f69a5f76acc3c74feb8b41
Evgenii Guryanov <whats.eternal@gmail.com>
2019-07-17 15:25:29 +0300

    codebox: prop dialog and insert

--- future/src/ct/ct_actions_edit.cc
+++ future/src/ct/ct_actions_edit.cc
@@ -66,9 +66,41 @@ void CtActions::table_handle()
     // todo:
 }
 
+// Insert Code Box
 void CtActions::codebox_handle()
 {
-    // todo:
+    if (!_node_sel_and_rich_text()) return;
+    if (!_is_curr_node_not_read_only_or_error()) return;
+
+    Glib::ustring textContent, justification;
+    Gtk::TextIter iter_sel_start, iter_sel_end;
+    if (curr_buffer()->get_has_selection())
+    {
+        curr_buffer()->get_selection_bounds(iter_sel_start, iter_sel_end);
+        textContent = iter_sel_start.get_text(iter_sel_end);
+    }
+    if (!ct_dialogs::codeboxhandle_dialog(*_pCtMainWin, _("Insert a CodeBox")))
+        return;
+
+    if (!textContent.empty())
+        curr_buffer()->erase(iter_sel_start, iter_sel_end);
+
+    Gtk::TextIter iter_insert = curr_buffer()->get_insert()->get_iter();
+
+    CtCodebox* pCtCodebox = new CtCodebox(textContent,
+                                          CtApp::P_ctCfg->codeboxSynHighl,
+                                          (int)CtApp::P_ctCfg->codeboxWidth,
+                                          (int)CtApp::P_ctCfg->codeboxHeight,
+                                          iter_insert.get_offset(),
+                                          justification);
+    pCtCodebox->setWidthInPixels(CtApp::P_ctCfg->codeboxWidthPixels);
+    pCtCodebox->setHighlightBrackets(CtApp::P_ctCfg->codeboxMatchBra);
+    pCtCodebox->setShowLineNumbers(CtApp::P_ctCfg->codeboxLineNum);
+    Glib::RefPtr<Gsv::Buffer> gsv_buffer = Glib::RefPtr<Gsv::Buffer>::cast_dynamic(curr_buffer());
+    pCtCodebox->insertInTextBuffer(gsv_buffer);
+
+    getCtMainWin()->get_tree_store().addAnchoredWidgets(getCtMainWin()->curr_tree_iter(),
+        {pCtCodebox}, &getCtMainWin()->get_text_view());
 }
 
 void CtActions::embfile_insert()
