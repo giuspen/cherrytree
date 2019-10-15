1f4a296dd617e3aa4eb42e32c668515ca83912d2
Giuseppe Penone <giuspen@gmail.com>
2019-10-15 03:25:51 +0100

    Collapse all nodes at startup doesn't work with expand at mouse click (#569)

--- modules/config.py
+++ modules/config.py
@@ -789,7 +789,7 @@ def set_tree_path_and_cursor_pos(dad):
             dad.sourceview.grab_focus()
             dad.curr_buffer.place_cursor(dad.curr_buffer.get_iter_at_offset(dad.cursor_position))
             dad.sourceview.scroll_to_mark(dad.curr_buffer.get_insert(), cons.SCROLL_MARGIN)
-            if dad.tree_click_expand:
+            if dad.tree_click_expand is True and dad.rest_exp_coll != 2:
                 dad.treeview.expand_row(dad.node_path, open_all=False)
     else: node_iter_to_focus = None
     if not node_iter_to_focus:
