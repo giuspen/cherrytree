6c3f53cca983dbf97aa4d322c818b451ddf0520b
Giuseppe Penone <giuspen@gmail.com>
2020-01-09 08:10:43 +0000

    0.38.11

--- modules/cons.py
+++ modules/cons.py
@@ -24,7 +24,7 @@ import sys
 import tempfile
 
 
-VERSION = "0.38.10"
+VERSION = "0.38.11"
 APP_NAME = "cherrytree"
 NEWER_VERSION_URL = "http://www.giuspen.com/software/version_cherrytree"
 IS_MAC_OS = False
