b8c6e690db8518930335620d90f5b095252e185a
Alex Life <letto4135@gmail.com>
2020-02-14 22:41:54 -0500

    fixing smart quote bug

--- modules/pgsc_spellcheck.py
+++ modules/pgsc_spellcheck.py
@@ -383,6 +383,9 @@ class SpellChecker(object):
         word_start = start.copy()
         if word_start.get_line() != 0 and word_start.get_char() == '“':
             word_start.forward_char()
+        elif word_start.get_line() == 0 and word_start.get_char() == '“':
+            if word_start.backward_char():
+                word_start.forward_chars(2)
         while word_start.compare(end) < 0:
             word_end = word_start.copy()
             word_end.forward_word_end()
