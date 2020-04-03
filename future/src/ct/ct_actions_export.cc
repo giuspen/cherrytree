1bf2054f187cbe67546c9063759ca08fe31b8a19
Evgenii Guryanov <whats.eternal@gmail.com>
2020-04-03 15:29:05 +0300

    backport 'Improve UX for Exported HTML #636'

--- future/src/ct/ct_actions_export.cc
+++ future/src/ct/ct_actions_export.cc
@@ -130,37 +130,24 @@ void CtActions::_export_to_html(Glib::ustring auto_path, bool auto_overwrite)
     if (export_type == CtDialogs::CtProcessNode::NONE) return;
 
     CtExport2Html export2html(_pCtMainWin);
+    Glib::ustring ret_html_path;
     if (export_type == CtDialogs::CtProcessNode::CURRENT_NODE)
     {
         Glib::ustring folder_name = CtMiscUtil::get_node_hierarchical_name(_pCtMainWin->curr_tree_iter());
-        if (export2html.prepare_html_folder("", folder_name, false))
+        if (export2html.prepare_html_folder("", folder_name, false, ret_html_path))
             export2html.node_export_to_html(_pCtMainWin->curr_tree_iter(), _export_options, "", -1, -1);
     }
     else if (export_type == CtDialogs::CtProcessNode::CURRENT_NODE_AND_SUBNODES)
     {
         Glib::ustring folder_name = CtMiscUtil::get_node_hierarchical_name(_pCtMainWin->curr_tree_iter());
-        if (export2html.prepare_html_folder("", folder_name, false))
-        {
+        if (export2html.prepare_html_folder("", folder_name, false, ret_html_path))
             export2html.nodes_all_export_to_html(false, _export_options);
-            // todo: why is it here? need to remove?
-            if (_pCtMainWin->get_curr_doc_file_type() == CtDocType::SQLite && _pCtMainWin->curr_tree_iter())
-            {
-                _pCtMainWin->get_state_machine().update_state();
-            }
-        }
     }
     else if (export_type == CtDialogs::CtProcessNode::ALL_TREE)
     {
         Glib::ustring folder_name = _pCtMainWin->get_curr_doc_file_name();
-        if (export2html.prepare_html_folder(auto_path, folder_name, auto_overwrite))
-        {
+        if (export2html.prepare_html_folder(auto_path, folder_name, auto_overwrite, ret_html_path))
             export2html.nodes_all_export_to_html(true, _export_options);
-            // todo: why is it here? need to remove?
-            if (_pCtMainWin->get_curr_doc_file_type() == CtDocType::SQLite && _pCtMainWin->curr_tree_iter())
-            {
-                _pCtMainWin->get_state_machine().update_state();
-            }
-        }
     }
     else if (export_type == CtDialogs::CtProcessNode::SELECTED_TEXT)
     {
@@ -169,9 +156,11 @@ void CtActions::_export_to_html(Glib::ustring auto_path, bool auto_overwrite)
         _curr_buffer()->get_selection_bounds(iter_start, iter_end);
 
         Glib::ustring folder_name = CtMiscUtil::get_node_hierarchical_name(_pCtMainWin->curr_tree_iter());
-        if (export2html.prepare_html_folder("", folder_name, false))
+        if (export2html.prepare_html_folder("", folder_name, false, ret_html_path))
             export2html.node_export_to_html(_pCtMainWin->curr_tree_iter(), _export_options, "", iter_start.get_offset(), iter_end.get_offset());
     }
+    if (!ret_html_path.empty())
+       CtFileSystem::external_folderpath_open(ret_html_path);
 }
 
 // Export To Plain Text Multiple (or single) Files
