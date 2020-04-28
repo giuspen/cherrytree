f6e22ab7d8d2204fc347627908b02d9d0c6f7eda
Giuseppe Penone <giuseppe.penone@flir.com>
2020-04-28 22:07:03 +0100

    working on gtkmm linux packaging

--- future/autogen.sh
+++ future/autogen.sh
@@ -7,6 +7,4 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
 autoreconf --force --install --verbose "$srcdir"
-echo "Running intltoolize --copy --force --automake"
-intltoolize --copy --force --automake
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
