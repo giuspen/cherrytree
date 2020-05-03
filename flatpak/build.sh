1b8db2b6f1d135f319db46b6ae512478f9c038b1
Giuseppe Penone <giuseppe.penone@flir.com>
2020-05-03 20:41:30 +0100

    packaging fixes

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 flatpak install flathub org.freedesktop.Platform//19.08 org.freedesktop.Sdk//19.08
-flatpak-builder --force-clean --arch=x86_64 build-dir com.giuspen.CherryTree.json
-flatpak-builder --run build-dir com.giuspen.CherryTree.json cherrytree
-#flatpak-builder --force-clean --arch=x86_64 --repo=repo build-dir com.giuspen.CherryTree.json
-#flatpak build-bundle --arch=x86_64 repo CherryTree-0.39.3.x86_64.flatpak com.giuspen.CherryTree
-#flatpak install CherryTree-0.39.3.x86_64.flatpak
+flatpak-builder --force-clean --arch=x86_64 build-dir com.giuspen.cherrytree.json
+flatpak-builder --run build-dir com.giuspen.cherrytree.json cherrytree
+#flatpak-builder --force-clean --arch=x86_64 --repo=repo build-dir com.giuspen.cherrytree.json
+#flatpak build-bundle --arch=x86_64 repo cherrytree-0.39.3.x86_64.flatpak com.giuspen.cherrytree
+#flatpak install cherrytree-0.39.3.x86_64.flatpak
