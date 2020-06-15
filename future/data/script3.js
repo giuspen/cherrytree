72cb4de97e0d025316f25fabb82959f2a7e03865
ForeverRainbow <foreverrainbow3@gmail.com>
2020-06-16 00:07:07 +0100

    Restyle of html output

--- future/data/script3.js
+++ future/data/script3.js
@@ -7,7 +7,7 @@ function toggleSubTree(element){
 	var nextSibling = element.parentElement.nextElementSibling;
 	if(nextSibling.tagName == 'UL'){
 		nextSibling.classList.toggle('hide');
-		if(element.textContent == '+'){
+		if(element.textContent === '+'){
 			element.textContent = '-'
 		}else{
 			element.textContent = '+'
@@ -33,9 +33,29 @@ function collapseAllSubtrees(element){
 	}
 }
 
+
+function get_tree() {
+	return document.getElementsByClassName("tree")[0]
+}
+
+
 window.onload = function(){ 
 	var show_page = window.location.hash.substr(1);
 	if (show_page !== '') {
 		changeFrame(show_page);
 	}
+
+	// Unwrap the header
+	var tree = get_tree()
+	var para = tree.children[0]
+
+	var index_txt = para.children[0].innerHTML
+	var index_title = document.createElement("h1")
+	index_title.innerHTML = index_txt
+	index_title.id = 'index_header'
+	tree.insertBefore(index_title, tree.children[0])
+
+
+	// Remove unwrapped elements
+	para.removeChild(para.children[0])
 }
