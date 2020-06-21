f980e54cd444fd06f4ee1563184ef75cd73ba59e
Evgenii Gurianov <whats.eternal@gmail.com>
2020-06-21 18:00:22 +0300

    fix test

--- future/tests/tests_misc_utils.cpp
+++ future/tests/tests_misc_utils.cpp
@@ -325,7 +325,7 @@ TEST(MiscUtilsGroup, mime__type_contains)
     CHECK(CtMiscUtil::mime_type_contains(unitTestsDataDir+"/mimetype_html.html", "html"));
 
 // test doesn't work on WIN32 and MacOS (Travis)
-#ifdef !(defined(_WIN32) || (defined(_TRAVIS) && defined(__APPLE__)))
+#if !(defined(_WIN32) || (defined(_TRAVIS) && defined(__APPLE__)))
     CHECK(CtMiscUtil::mime_type_contains(unitTestsDataDir+"/mimetype_cpp.cpp", "text/"));
 #endif
 
