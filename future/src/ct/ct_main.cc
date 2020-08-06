06713e3ae35f5d3e6e820f2885dd50b97f4b9226
txe <whats.eternal@gmail.com>
2020-08-07 00:32:52 +0300

    adds rotating log file for windows (#1092)

--- future/src/ct/ct_main.cc
+++ future/src/ct/ct_main.cc
@@ -26,10 +26,11 @@
 #include "config.h"
 #include "ct_logging.h"
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
 void glib_log_handler(const gchar*, GLogLevelFlags log_level, const gchar* msg, gpointer)
 {
-    auto gtk_logger = spdlog::get("GTK Logger");
+    auto gtk_logger = spdlog::get("gtk");
     switch (log_level) {
         case G_LOG_LEVEL_ERROR:
             gtk_logger->error(msg);
@@ -48,7 +49,7 @@ void glib_log_handler(const gchar*, GLogLevelFlags log_level, const gchar* msg, 
             break;
         case G_LOG_LEVEL_DEBUG:
             // disable due to excessive output
-            // gtk_logger->debug(msg);
+            //gtk_logger->debug(msg);
             break;
         default:
             gtk_logger->info(msg);
@@ -74,14 +75,37 @@ int main(int argc, char *argv[])
     textdomain(GETTEXT_PACKAGE);
 #endif
 
-    // Setup spdlog, use debug level by default for now
-    spdlog::set_level(spdlog::level::debug);
 
-    // Redirect Gtk log messages to spdlog
-    auto gtk_logger = spdlog::stdout_color_mt("GTK Logger");
-    g_log_set_default_handler(glib_log_handler, nullptr);
+    // output logs into console and a log file
+    std::vector<spdlog::sink_ptr> sinks;
+    sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    try
+    {
+#ifdef _WIN32
+        // Create a file rotating logger with 5mb size max and 3 rotated files
+        auto max_size = 1048576 * 5;
+        auto max_files = 3;
+        fs::path log_path = fs::get_cherrytree_configdir() / "cherrytree.log";
+        sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_path.c_str(), max_size, max_files));
+#endif
+    }
+    catch (const spdlog::spdlog_ex &ex)
+    {
+        spdlog::debug("Log init failed: {}", ex.what());
+    }
+
+    spdlog::drop(""); // remove the default logger (if you want, you can use its name)
+    // these two loggers are the same, they just add "[  ]" and "[gtk]" in their output
+    auto cherrytree_logger = std::make_shared<spdlog::logger>("   ", begin(sinks), end(sinks));
+    auto gtk_logger = std::make_shared<spdlog::logger>("gtk", begin(sinks), end(sinks));
+
+    spdlog::set_default_logger(cherrytree_logger);         // make our logger as a default logger
+    spdlog::register_logger(gtk_logger);                   // register it, so we can access it in another place
+    spdlog::flush_on(spdlog::level::debug);                // flush when "info" or higher message is logged on all loggers
+    spdlog::set_level(spdlog::level::debug);               // Setup spdlog, use debug level by default for now
+
+    g_log_set_default_handler(glib_log_handler, nullptr);  // Redirect Gtk log messages to spdlog
 
     Glib::RefPtr<CtApp> r_app = CtApp::create();
-
     return r_app->run(argc, argv);
 }
