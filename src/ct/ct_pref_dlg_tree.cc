dd3addfd786154d18713c026d5f7d23ce4efd7be
Giuseppe Penone <giuspen@gmail.com>
2021-03-07 21:32:21 +0000

    improved dialog to select an item in the list focusing the item that is currently in use

--- src/ct/ct_pref_dlg_tree.cc
+++ src/ct/ct_pref_dlg_tree.cc
@@ -28,20 +28,19 @@ Gtk::Widget* CtPrefDlg::build_tab_tree()
 {
     CtConfig* pConfig = _pCtMainWin->get_ct_config();
 
-    Gtk::VBox* vbox_nodes_icons = Gtk::manage(new Gtk::VBox());
+    auto vbox_nodes_icons = Gtk::manage(new Gtk::Box{Gtk::ORIENTATION_VERTICAL});
 
-    Gtk::RadioButton* radiobutton_node_icon_cherry = Gtk::manage(new Gtk::RadioButton(_("Use Different Cherries per Level")));
-    Gtk::RadioButton* radiobutton_node_icon_custom = Gtk::manage(new Gtk::RadioButton(_("Use Selected Icon")));
+    Gtk::RadioButton* radiobutton_node_icon_cherry = Gtk::manage(new Gtk::RadioButton{_("Use Different Cherries per Level")});
+    Gtk::RadioButton* radiobutton_node_icon_custom = Gtk::manage(new Gtk::RadioButton{_("Use Selected Icon")});
     radiobutton_node_icon_custom->join_group(*radiobutton_node_icon_cherry);
-    Gtk::RadioButton* radiobutton_node_icon_none = Gtk::manage(new Gtk::RadioButton(_("No Icon")));
+    Gtk::RadioButton* radiobutton_node_icon_none = Gtk::manage(new Gtk::RadioButton{_("No Icon")});
     radiobutton_node_icon_none->join_group(*radiobutton_node_icon_cherry);
-    Gtk::CheckButton* checkbutton_aux_icon_hide = Gtk::manage(new Gtk::CheckButton(_("Hide Right Side Auxiliary Icon")));
+    Gtk::CheckButton* checkbutton_aux_icon_hide = Gtk::manage(new Gtk::CheckButton{_("Hide Right Side Auxiliary Icon")});
     checkbutton_aux_icon_hide->set_active(pConfig->auxIconHide);
 
-    Gtk::Button* c_icon_button = Gtk::manage(new Gtk::Button());
+    auto c_icon_button = Gtk::manage(new Gtk::Button{});
     c_icon_button->set_image(*_pCtMainWin->new_image_from_stock(CtConst::NODE_CUSTOM_ICONS.at(pConfig->defaultIconText), Gtk::ICON_SIZE_BUTTON));
-    Gtk::HBox* c_icon_hbox = Gtk::manage(new Gtk::HBox());
-    c_icon_hbox->set_spacing(2);
+    auto c_icon_hbox = Gtk::manage(new Gtk::Box{Gtk::ORIENTATION_HORIZONTAL, 2/*spacing*/});
     c_icon_hbox->pack_start(*radiobutton_node_icon_custom, false, false);
     c_icon_hbox->pack_start(*c_icon_button, false, false);
 
@@ -55,14 +54,14 @@ Gtk::Widget* CtPrefDlg::build_tab_tree()
     radiobutton_node_icon_custom->set_active(pConfig->nodesIcons == "b");
     radiobutton_node_icon_none->set_active(pConfig->nodesIcons == "n");
 
-    Gtk::VBox* vbox_nodes_startup = Gtk::manage(new Gtk::VBox());
+    auto vbox_nodes_startup = Gtk::manage(new Gtk::Box{Gtk::ORIENTATION_VERTICAL});
 
-    Gtk::RadioButton* radiobutton_nodes_startup_restore = Gtk::manage(new Gtk::RadioButton(_("Restore Expanded/Collapsed Status")));
-    Gtk::RadioButton* radiobutton_nodes_startup_expand = Gtk::manage(new Gtk::RadioButton(_("Expand all Nodes")));
+    Gtk::RadioButton* radiobutton_nodes_startup_restore = Gtk::manage(new Gtk::RadioButton{_("Restore Expanded/Collapsed Status")});
+    Gtk::RadioButton* radiobutton_nodes_startup_expand = Gtk::manage(new Gtk::RadioButton{_("Expand all Nodes")});
     radiobutton_nodes_startup_expand->join_group(*radiobutton_nodes_startup_restore);
-    Gtk::RadioButton* radiobutton_nodes_startup_collapse = Gtk::manage(new Gtk::RadioButton(_("Collapse all Nodes")));
+    Gtk::RadioButton* radiobutton_nodes_startup_collapse = Gtk::manage(new Gtk::RadioButton{_("Collapse all Nodes")});
     radiobutton_nodes_startup_collapse->join_group(*radiobutton_nodes_startup_restore);
-    Gtk::CheckButton* checkbutton_nodes_bookm_exp = Gtk::manage(new Gtk::CheckButton(_("Nodes in Bookmarks Always Visible")));
+    Gtk::CheckButton* checkbutton_nodes_bookm_exp = Gtk::manage(new Gtk::CheckButton{_("Nodes in Bookmarks Always Visible")});
     checkbutton_nodes_bookm_exp->set_active(pConfig->nodesBookmExp);
     checkbutton_nodes_bookm_exp->set_sensitive(pConfig->restoreExpColl != CtRestoreExpColl::ALL_EXP);
 
@@ -76,28 +75,26 @@ Gtk::Widget* CtPrefDlg::build_tab_tree()
     radiobutton_nodes_startup_expand->set_active(pConfig->restoreExpColl == CtRestoreExpColl::ALL_EXP);
     radiobutton_nodes_startup_collapse->set_active(pConfig->restoreExpColl == CtRestoreExpColl::ALL_COLL);
 
-    Gtk::VBox* vbox_misc_tree = Gtk::manage(new Gtk::VBox());
-    Gtk::HBox* hbox_tree_nodes_names_width = Gtk::manage(new Gtk::HBox());
-    hbox_tree_nodes_names_width->set_spacing(4);
-    Gtk::CheckButton* checkbutton_tree_nodes_names_wrap_ena = Gtk::manage(new Gtk::CheckButton(_("Tree Nodes Names Wrapping Width")));
+    auto vbox_misc_tree = Gtk::manage(new Gtk::Box{Gtk::ORIENTATION_VERTICAL});
+    auto hbox_tree_nodes_names_width = Gtk::manage(new Gtk::Box{Gtk::ORIENTATION_HORIZONTAL, 4/*spacing*/});
+    Gtk::CheckButton* checkbutton_tree_nodes_names_wrap_ena = Gtk::manage(new Gtk::CheckButton{_("Tree Nodes Names Wrapping Width")});
     checkbutton_tree_nodes_names_wrap_ena->set_active(pConfig->cherryWrapEnabled);
     Glib::RefPtr<Gtk::Adjustment> adj_tree_nodes_names_width = Gtk::Adjustment::create(pConfig->cherryWrapWidth, 10, 10000, 1);
-    Gtk::SpinButton* spinbutton_tree_nodes_names_width = Gtk::manage(new Gtk::SpinButton(adj_tree_nodes_names_width));
+    Gtk::SpinButton* spinbutton_tree_nodes_names_width = Gtk::manage(new Gtk::SpinButton{adj_tree_nodes_names_width});
     spinbutton_tree_nodes_names_width->set_value(pConfig->cherryWrapWidth);
     spinbutton_tree_nodes_names_width->set_sensitive(pConfig->cherryWrapEnabled);
     hbox_tree_nodes_names_width->pack_start(*checkbutton_tree_nodes_names_wrap_ena, false, false);
     hbox_tree_nodes_names_width->pack_start(*spinbutton_tree_nodes_names_width, false, false);
-    Gtk::CheckButton* checkbutton_tree_right_side = Gtk::manage(new Gtk::CheckButton(_("Display Tree on the Right Side")));
+    Gtk::CheckButton* checkbutton_tree_right_side = Gtk::manage(new Gtk::CheckButton{_("Display Tree on the Right Side")});
     checkbutton_tree_right_side->set_active(pConfig->treeRightSide);
-    Gtk::CheckButton* checkbutton_tree_click_focus_text = Gtk::manage(new Gtk::CheckButton(_("Move Focus to Text at Mouse Click")));
+    Gtk::CheckButton* checkbutton_tree_click_focus_text = Gtk::manage(new Gtk::CheckButton{_("Move Focus to Text at Mouse Click")});
     checkbutton_tree_click_focus_text->set_active(pConfig->treeClickFocusText);
-    Gtk::CheckButton* checkbutton_tree_click_expand = Gtk::manage(new Gtk::CheckButton(_("Expand Node at Mouse Click")));
+    Gtk::CheckButton* checkbutton_tree_click_expand = Gtk::manage(new Gtk::CheckButton{_("Expand Node at Mouse Click")});
     checkbutton_tree_click_expand->set_active(pConfig->treeClickExpand);
-    Gtk::HBox* hbox_nodes_on_node_name_header = Gtk::manage(new Gtk::HBox());
-    hbox_nodes_on_node_name_header->set_spacing(4);
-    Gtk::Label* label_nodes_on_node_name_header = Gtk::manage(new Gtk::Label(_("Last Visited Nodes on Node Name Header")));
+    auto hbox_nodes_on_node_name_header = Gtk::manage(new Gtk::Box{Gtk::ORIENTATION_HORIZONTAL, 4/*spacing*/});
+    Gtk::Label* label_nodes_on_node_name_header = Gtk::manage(new Gtk::Label{_("Last Visited Nodes on Node Name Header")});
     Glib::RefPtr<Gtk::Adjustment> adj_nodes_on_node_name_header = Gtk::Adjustment::create(pConfig->nodesOnNodeNameHeader, 0, 100, 1);
-    Gtk::SpinButton* spinbutton_nodes_on_node_name_header = Gtk::manage(new Gtk::SpinButton(adj_nodes_on_node_name_header));
+    Gtk::SpinButton* spinbutton_nodes_on_node_name_header = Gtk::manage(new Gtk::SpinButton{adj_nodes_on_node_name_header});
     spinbutton_nodes_on_node_name_header->set_value(pConfig->nodesOnNodeNameHeader);
     hbox_nodes_on_node_name_header->pack_start(*label_nodes_on_node_name_header, false, false);
     hbox_nodes_on_node_name_header->pack_start(*spinbutton_nodes_on_node_name_header, false, false);
@@ -109,8 +106,7 @@ Gtk::Widget* CtPrefDlg::build_tab_tree()
     vbox_misc_tree->pack_start(*hbox_nodes_on_node_name_header, false, false);
     Gtk::Frame* frame_misc_tree = new_managed_frame_with_align(_("Miscellaneous"), vbox_misc_tree);
 
-    Gtk::VBox* pMainBox = Gtk::manage(new Gtk::VBox());
-    pMainBox->set_spacing(3);
+    auto pMainBox = Gtk::manage(new Gtk::Box{Gtk::ORIENTATION_VERTICAL, 3/*spacing*/});
     pMainBox->set_margin_left(6);
     pMainBox->set_margin_top(6);
     pMainBox->pack_start(*frame_nodes_icons, false, false);
@@ -163,7 +159,11 @@ Gtk::Widget* CtPrefDlg::build_tab_tree()
         auto itemStore = CtChooseDialogListStore::create();
         for (int i = 1 /* skip 0 */; i < (int)CtConst::NODE_CUSTOM_ICONS.size(); ++i)
             itemStore->add_row(CtConst::NODE_CUSTOM_ICONS[i], std::to_string(i), "");
-        auto res = CtDialogs::choose_item_dialog(*this, _("Select Node Icon"), itemStore);
+        auto res = CtDialogs::choose_item_dialog(*this,
+                                                 _("Select Node Icon"),
+                                                 itemStore,
+                                                 nullptr,
+                                                 std::to_string(pConfig->defaultIconText-1));
         if (res) {
             pConfig->defaultIconText = std::stoi(res->get_value(itemStore->columns.key));
             c_icon_button->set_image(*_pCtMainWin->new_image_from_stock(res->get_value(itemStore->columns.stock_id), Gtk::ICON_SIZE_BUTTON));
