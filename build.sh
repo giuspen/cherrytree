d3bae863a8a2f1fc7867cf42bd2cc93bb905017b
Giuseppe Penone <giuspen@gmail.com>
2022-11-13 18:54:53 +0000

    fixed broken build script

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ MAKE_APPIMAGE=""
 [[ "${MSYSTEM}" =~ "MINGW" ]] && IS_MSYS2_BUILD="Y"
 [ -n "${IS_MSYS2_BUILD}" ] && DEFAULT_BUILD_TYPE="Release" || DEFAULT_BUILD_TYPE="Debug"
 
-if [ "${ARG_VAL_LOWER}" == "debug" ] || ["${ARG_VAL_LOWER}" == "dbg" ]
+if [ "${ARG_VAL_LOWER}" == "debug" ] || [ "${ARG_VAL_LOWER}" == "dbg" ]
 then
   CMAKE_BUILD_TYPE="Debug"
 elif [ "${ARG_VAL_LOWER}" == "release" ] || [ "${ARG_VAL_LOWER}" == "rel" ]
