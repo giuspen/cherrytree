bd8fc007c6d15fddd53dadfabb734fe92dab05b3
Giuseppe Penone <giuspen@gmail.com>
2022-11-21 22:11:22 +0000

    using github.com instead of giuspen.net to retrieve the latest version released (#2162)

--- src/ct/ct_actions_help.cc
+++ src/ct/ct_actions_help.cc
@@ -45,31 +45,40 @@ void CtActions::check_for_newer_version()
     statusbar.update_status(_("Checking for Newer Version..."));
     while (gtk_events_pending()) gtk_main_iteration();
 
-    std::string latest_version_from_server = str::trim(fs::download_file("https://www.giuspen.net/software/version_cherrytree"));
-    //g_print("v='%s'\n", latest_version_from_server.c_str());
-    if (latest_version_from_server.empty() or latest_version_from_server.size() > 10) {
+    std::string latest_debian_changelog_from_server = fs::download_file("https://raw.githubusercontent.com/giuspen/cherrytree/master/debian/changelog");
+    std::size_t openp = latest_debian_changelog_from_server.find("(");
+    std::size_t closep = latest_debian_changelog_from_server.find(")");
+    if (std::string::npos == openp or std::string::npos == closep or closep < openp) {
         statusbar.update_status(_("Failed to Retrieve Latest Version Information - Try Again Later"));
+        return;
+    }
+    std::string latest_version_from_server = latest_debian_changelog_from_server.substr(openp + 1, closep - openp - 1);
+    auto re = Glib::Regex::create("(\\d+)\\.(\\d+)\\.(\\d+)-\\d+");
+    Glib::MatchInfo match;
+    if (not re->match(latest_version_from_server, match)) {
+        statusbar.update_status(_("Failed to Retrieve Latest Version Information - Try Again Later"));
+        return;
+    }
+    std::vector<gint64> splitted_latest_v = {atoi(match.fetch(1).c_str()), atoi(match.fetch(2).c_str()), atoi(match.fetch(3).c_str())};
+    std::vector<gint64> splitted_local_v = CtStrUtil::gstring_split_to_int64(PACKAGE_VERSION, ".");
+    if (splitted_local_v.size() != 3) {
+        spdlog::error("unexpected version {}", PACKAGE_VERSION);
     }
     else {
-        std::vector<gint64> splitted_latest_v = CtStrUtil::gstring_split_to_int64(latest_version_from_server.c_str(), ".");
-        std::vector<gint64> splitted_local_v = CtStrUtil::gstring_split_to_int64(PACKAGE_VERSION, ".");
-        if (splitted_latest_v.size() != 3 or splitted_local_v.size() != 3) {
-            g_critical("unexpected versions %s, %s", latest_version_from_server.c_str(), PACKAGE_VERSION);
+        gint64 weighted_latest_v = splitted_latest_v[0]*10000 + splitted_latest_v[1]*100 + splitted_latest_v[2];
+        gint64 weighted_local_v = splitted_local_v[0]*10000 + splitted_local_v[1]*100 + splitted_local_v[2];
+        std::string trail_latest_from_srv = str::format(" ({}.{}.{})", splitted_latest_v[0], splitted_latest_v[1], splitted_latest_v[2]);
+        if (weighted_latest_v > weighted_local_v) {
+            CtDialogs::info_dialog(Glib::ustring{_("A Newer Version Is Available!")} + trail_latest_from_srv, *_pCtMainWin);
+            _pCtMainWin->update_selected_node_statusbar_info();
         }
         else {
-            gint64 weighted_latest_v = splitted_latest_v[0]*10000 + splitted_latest_v[1]*100 + splitted_latest_v[2];
-            gint64 weighted_local_v = splitted_local_v[0]*10000 + splitted_local_v[1]*100 + splitted_local_v[2];
-            if (weighted_latest_v > weighted_local_v) {
-                CtDialogs::info_dialog(Glib::ustring{_("A Newer Version Is Available!")} + " (" + latest_version_from_server + ")", *_pCtMainWin);
-                _pCtMainWin->update_selected_node_statusbar_info();
+            if (weighted_latest_v == weighted_local_v) {
+                statusbar.update_status(Glib::ustring{_("You Are Using the Latest Version Available")} + trail_latest_from_srv);
             }
             else {
-                if (weighted_latest_v == weighted_local_v) {
-                    statusbar.update_status(Glib::ustring{_("You Are Using the Latest Version Available")} + " (" + latest_version_from_server + ")");
-                }
-                else {
-                    statusbar.update_status(_("You Are Using a Development Version"));
-                }
+                statusbar.update_status(_("You Are Using a Development Version"));
+                spdlog::debug("latest from server {}.{}.{}", splitted_latest_v[0], splitted_latest_v[1], splitted_latest_v[2]);
             }
         }
     }
