2827e3fb03756df87caca8ce70af4086a7c992c1
Giuseppe Penone <giuspen@gmail.com>
2023-09-28 21:03:17 +0100

    fixed './build.sh appimage'

--- build.sh
+++ build.sh
@@ -136,11 +136,10 @@ fi
 if [ -n "${MAKE_APPIMAGE}" ]
 then
   # https://github.com/linuxdeploy/linuxdeploy-plugin-gtk
-  [ -f linuxdeploy-plugin-gtk.sh ] || wget -c "https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh"
+  [ -f linuxdeploy-plugin-gtk.sh ] || wget -c "https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/97290a3e374381e9fa7983ba098729a37076f3ed/linuxdeploy-plugin-gtk.sh"
   [ -f linuxdeploy-x86_64.AppImage ] || wget -c "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
   [ -f TinyTeX-1-v2022.04.04.tar.xz ] || wget -c "https://www.giuspen.net/software/TinyTeX-1-v2022.04.04.tar.xz"
-  chmod +x linuxdeploy-plugin-gtk.sh
-  chmod +x linuxdeploy-x86_64.AppImage
+  chmod +x linuxdeploy-x86_64.AppImage linuxdeploy-plugin-gtk.sh
   rm -rf AppDir
   mkdir AppDir
   tar xf TinyTeX-1-v2022.04.04.tar.xz -C AppDir/
