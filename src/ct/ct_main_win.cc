0e3eb980e4194fa87d358a20f6f9a4a2acd6f9a3
Giuseppe Penone <giuspen@gmail.com>
2023-10-20 20:58:43 +0100

    fixed progress bar of search hardly visible on windows (#2373)

--- src/ct/ct_main_win.cc
+++ src/ct/ct_main_win.cc
@@ -377,6 +377,9 @@ void CtMainWin::update_theme()
     css_str += ".ct-header-panel { background-color: " + _pCtConfig->ttDefBg + "; } ";
     css_str += ".ct-header-panel button { margin: 2px; padding: 0 4px 0 4px; } ";
     css_str += ".ct-status-bar bar { margin: 0px; } ";
+#if defined(_WIN32)
+    css_str += ".ct-status-bar progressbar trough, progressbar progress { min-height: 16px; } ";
+#endif // _WIN32 - without this the progress bar height is 1 or 2 px, hardly visible
     css_str += ".ct-table-header-cell { font-weight: bold; } ";
     css_str += ".ct-table grid { background: #cccccc; border-style:solid; border-width: 1px; border-color: gray; } ";
     css_str += "toolbar { padding: 2px 2px 2px 2px; } ";
