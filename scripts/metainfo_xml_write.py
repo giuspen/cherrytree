5beca28d485fd807bea8fa177665ccd29e57a70b
Giuseppe Penone <giuspen@gmail.com>
2023-11-03 21:07:29 +0000

    fixed invalid escape sequence in python script

--- scripts/metainfo_xml_write.py
+++ scripts/metainfo_xml_write.py
@@ -16,7 +16,7 @@ DEBIAN_CHANGELOG_PATH = os.path.join(ROOT_DIR, "debian", "changelog")
 VERSION = "?"
 with open(DEBIAN_CHANGELOG_PATH, "rb") as fd:
     for fileline in fd:
-        match = re.search(b"cherrytree +\(([0-9]+\.[0-9]+\.[0-9]+)[-+]", fileline)
+        match = re.search(b"cherrytree +\\(([0-9]+\\.[0-9]+\\.[0-9]+)[-+]", fileline)
         if match is not None:
             VERSION = str(match.group(1))
             #print(VERSION)
