c29cab476513430740238a56a515af8e7826852b
Giuseppe Penone <giuspen@gmail.com>
2025-05-17 21:37:20 +0100

    setting up github workflow for signing msys2 build output (#2651)

--- scripts/msys2_prepare_standalone.sh
+++ scripts/msys2_prepare_standalone.sh
@@ -4,16 +4,16 @@ set -e
 # and in particular make-gedit-installer.sh
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-GIT_CT_FOLDER="$(dirname "${SCRIPT_DIR}")"
-GIT_CT_EXE="${GIT_CT_FOLDER}/build/cherrytree.exe"
-GIT_CT_LANGUAGES_FOLDER="${GIT_CT_FOLDER}/po"
-GIT_CT_DATA_FOLDER="${GIT_CT_FOLDER}/data"
-GIT_CT_ICONS_FOLDER="${GIT_CT_FOLDER}/icons"
-GIT_CT_LANGUAGE_SPECS_FOLDER="${GIT_CT_FOLDER}/language-specs"
-GIT_CT_STYLES_FOLDER="${GIT_CT_FOLDER}/styles"
-GIT_CT_LICENSE="${GIT_CT_FOLDER}/license.txt"
-GIT_CT_HUNSPELL="${GIT_CT_FOLDER}/hunspell"
-GIT_CT_CONFIG_H="${GIT_CT_FOLDER}/config.h"
+IN_CT_FOLDER="$(dirname "${SCRIPT_DIR}")"
+IN_CT_EXE="${IN_CT_FOLDER}/build/cherrytree.exe"
+IN_CT_LANGUAGES_FOLDER="${IN_CT_FOLDER}/po"
+IN_CT_DATA_FOLDER="${IN_CT_FOLDER}/data"
+IN_CT_ICONS_FOLDER="${IN_CT_FOLDER}/icons"
+IN_CT_LANGUAGE_SPECS_FOLDER="${IN_CT_FOLDER}/language-specs"
+IN_CT_STYLES_FOLDER="${IN_CT_FOLDER}/styles"
+IN_CT_LICENSE="${IN_CT_FOLDER}/license.txt"
+IN_CT_HUNSPELL="${IN_CT_FOLDER}/hunspell"
+IN_CT_CONFIG_H="${IN_CT_FOLDER}/config.h"
 OLD_UCRT64_FOLDER="/ucrt64"
 DOWNGRADE_PACKAGE_LOCATION="https://repo.msys2.org/mingw/ucrt64"
 DOWNGRADE_PACKAGE_NAME=""
@@ -24,19 +24,19 @@ then
   pacman -U --noconfirm ${DOWNGRADE_PACKAGE_NAME}
 fi
 echo "build..."
-cd ${GIT_CT_FOLDER}
+cd ${IN_CT_FOLDER}
 ./build.sh release
 
 
-CT_VERSION_NUM="$(cat ${GIT_CT_CONFIG_H} | grep PACKAGE_VERSION_WINDOWS_STR | awk '{print substr($3, 2, length($3)-2)}')"
-NEW_MSYS2_FOLDER="C:/Users/${USER}/Desktop/cherrytree-msys2"
-NEW_ROOT_FOLDER="C:/Users/${USER}/Desktop/cherrytree_${CT_VERSION_NUM}_win64_portable"
-NEW_ROOT_FOLDER_NOLATEX="${NEW_ROOT_FOLDER}_nolatex"
-NEW_UCRT64_FOLDER="${NEW_ROOT_FOLDER}/ucrt64"
-NEW_ETC_GTK_FOLDER="${NEW_ROOT_FOLDER}/etc/gtk-3.0"
-NEW_ETC_GTK_SETTINGS_INI="${NEW_ETC_GTK_FOLDER}/settings.ini"
-NEW_HUNSPELL_FOLDER="${NEW_UCRT64_FOLDER}/share/hunspell"
-NEW_CHERRYTREE_SHARE="${NEW_UCRT64_FOLDER}/usr/share/cherrytree"
+CT_VERSION_NUM="$(cat ${IN_CT_CONFIG_H} | grep PACKAGE_VERSION_WINDOWS_STR | awk '{print substr($3, 2, length($3)-2)}')"
+OUT_MSYS2_FOLDER="${IN_CT_FOLDER}/build/cherrytree-msys2"
+OUT_ROOT_FOLDER="${IN_CT_FOLDER}/build/cherrytree_${CT_VERSION_NUM}_win64_portable"
+OUT_ROOT_FOLDER_NOLATEX="${OUT_ROOT_FOLDER}_nolatex"
+OUT_UCRT64_FOLDER="${OUT_ROOT_FOLDER}/ucrt64"
+OUT_ETC_GTK_FOLDER="${OUT_ROOT_FOLDER}/etc/gtk-3.0"
+OUT_ETC_GTK_SETTINGS_INI="${OUT_ETC_GTK_FOLDER}/settings.ini"
+OUT_HUNSPELL_FOLDER="${OUT_UCRT64_FOLDER}/share/hunspell"
+OUT_CHERRYTREE_SHARE="${OUT_UCRT64_FOLDER}/usr/share/cherrytree"
 
 
 # latex.exe and dvipng.exe ensure the list of files to copy from is available
@@ -75,20 +75,20 @@ ls -la ${OLD_UCRT64_FOLDER}/var/lib/texmf/fonts/map/dvips/updmap/ps2pk.map
 
 
 echo "cleanup old runs..."
-for folderpath in ${NEW_MSYS2_FOLDER} ${NEW_ROOT_FOLDER}
+for folderpath in ${OUT_MSYS2_FOLDER} ${OUT_ROOT_FOLDER}
 do
   [ -d ${folderpath} ] && rm -rfv ${folderpath}
 done
 
 
-echo "installing minimal filesystem to ${NEW_MSYS2_FOLDER}..."
-mkdir -p ${NEW_MSYS2_FOLDER}
-pushd ${NEW_MSYS2_FOLDER} > /dev/null
+echo "installing minimal filesystem to ${OUT_MSYS2_FOLDER}..."
+mkdir -p ${OUT_MSYS2_FOLDER}
+pushd ${OUT_MSYS2_FOLDER} > /dev/null
 mkdir -p var/lib/pacman
 mkdir -p var/log
 mkdir -p tmp
-pacman -Syu --root ${NEW_MSYS2_FOLDER}
-pacman -S --noconfirm --root ${NEW_MSYS2_FOLDER} \
+pacman -Syu --root ${OUT_MSYS2_FOLDER}
+pacman -S --noconfirm --root ${OUT_MSYS2_FOLDER} \
   filesystem \
   bash \
   pacman \
@@ -104,13 +104,13 @@ pacman -S --noconfirm --root ${NEW_MSYS2_FOLDER} \
   mingw-w64-ucrt-x86_64-spdlog
 _result=$?
 if [ "$_result" -ne "0" ]; then
-  echo "failed to create base data via command 'pacman -S <packages names list> --noconfirm --root ${NEW_MSYS2_FOLDER}'"
+  echo "failed to create base data via command 'pacman -S <packages names list> --noconfirm --root ${OUT_MSYS2_FOLDER}'"
   exit 1
 fi
 if [ -n "${DOWNGRADE_PACKAGE_NAME}" ]
 then
   wget "${DOWNGRADE_PACKAGE_LOCATION}/${DOWNGRADE_PACKAGE_NAME}"
-  pacman -U --noconfirm --root ${NEW_MSYS2_FOLDER} ${DOWNGRADE_PACKAGE_NAME}
+  pacman -U --noconfirm --root ${OUT_MSYS2_FOLDER} ${DOWNGRADE_PACKAGE_NAME}
   _result=$?
   if [ "$_result" -ne "0" ]; then
     echo "failed to downgrade package"
@@ -121,99 +121,99 @@ fi
 popd > /dev/null
 
 
-echo "moving over necessary files from ${NEW_MSYS2_FOLDER} to ${NEW_ROOT_FOLDER}..."
-mkdir -p ${NEW_ROOT_FOLDER}
+echo "moving over necessary files from ${OUT_MSYS2_FOLDER} to ${OUT_ROOT_FOLDER}..."
+mkdir -p ${OUT_ROOT_FOLDER}
 _result="1"
 while [ "$_result" -ne "0" ]
 do
   sleep 1
-  mv -v ${NEW_MSYS2_FOLDER}/ucrt64 ${NEW_ROOT_FOLDER}/
+  mv -v ${OUT_MSYS2_FOLDER}/ucrt64 ${OUT_ROOT_FOLDER}/
   _result=$?
 done
 
 
 echo "removing unnecessary files..."
 # remove .a files not needed for the installer
-find ${NEW_UCRT64_FOLDER} -name "*.a" -exec rm -f {} \;
+find ${OUT_UCRT64_FOLDER} -name "*.a" -exec rm -f {} \;
 # remove unneeded binaries
-find ${NEW_UCRT64_FOLDER} -not -name "g*.exe" -name "*.exe" -exec rm -f {} \;
-rm -rf ${NEW_UCRT64_FOLDER}/bin/2to3*
-rm -rf ${NEW_UCRT64_FOLDER}/bin/autopoint
-rm -rf ${NEW_UCRT64_FOLDER}/bin/idle*
-rm -rf ${NEW_UCRT64_FOLDER}/bin/bz*
-rm -rf ${NEW_UCRT64_FOLDER}/bin/xz*
-rm -rf ${NEW_UCRT64_FOLDER}/bin/gtk3-*.exe
-rm -rf ${NEW_UCRT64_FOLDER}/bin/*gettextize
-rm -rf ${NEW_UCRT64_FOLDER}/bin/*.sh
-rm -rf ${NEW_UCRT64_FOLDER}/bin/update-*
-rm -rf ${NEW_UCRT64_FOLDER}/bin/gdbm*.exe
-rm -rf ${NEW_UCRT64_FOLDER}/bin/py*
-rm -rf ${NEW_UCRT64_FOLDER}/bin/*-config
-rm -f ${NEW_UCRT64_FOLDER}/bin/tcl86.dll
-rm -f ${NEW_UCRT64_FOLDER}/bin/tk86.dll
-rm -rf ${NEW_UCRT64_FOLDER}/sbin
+find ${OUT_UCRT64_FOLDER} -not -name "g*.exe" -name "*.exe" -exec rm -f {} \;
+rm -rf ${OUT_UCRT64_FOLDER}/bin/2to3*
+rm -rf ${OUT_UCRT64_FOLDER}/bin/autopoint
+rm -rf ${OUT_UCRT64_FOLDER}/bin/idle*
+rm -rf ${OUT_UCRT64_FOLDER}/bin/bz*
+rm -rf ${OUT_UCRT64_FOLDER}/bin/xz*
+rm -rf ${OUT_UCRT64_FOLDER}/bin/gtk3-*.exe
+rm -rf ${OUT_UCRT64_FOLDER}/bin/*gettextize
+rm -rf ${OUT_UCRT64_FOLDER}/bin/*.sh
+rm -rf ${OUT_UCRT64_FOLDER}/bin/update-*
+rm -rf ${OUT_UCRT64_FOLDER}/bin/gdbm*.exe
+rm -rf ${OUT_UCRT64_FOLDER}/bin/py*
+rm -rf ${OUT_UCRT64_FOLDER}/bin/*-config
+rm -f ${OUT_UCRT64_FOLDER}/bin/tcl86.dll
+rm -f ${OUT_UCRT64_FOLDER}/bin/tk86.dll
+rm -rf ${OUT_UCRT64_FOLDER}/sbin
 # remove other useless folders/files
-rm -rf ${NEW_UCRT64_FOLDER}/var
-rm -rf ${NEW_UCRT64_FOLDER}/include
-rm -rf ${NEW_UCRT64_FOLDER}/libexec
-rm -rf ${NEW_UCRT64_FOLDER}/share/man
-rm -rf ${NEW_UCRT64_FOLDER}/share/readline
-rm -rf ${NEW_UCRT64_FOLDER}/share/info
-rm -rf ${NEW_UCRT64_FOLDER}/share/aclocal
-rm -rf ${NEW_UCRT64_FOLDER}/share/gnome-common
-rm -rf ${NEW_UCRT64_FOLDER}/share/glade
-rm -rf ${NEW_UCRT64_FOLDER}/share/gettext
-rm -rf ${NEW_UCRT64_FOLDER}/share/terminfo
-rm -rf ${NEW_UCRT64_FOLDER}/share/tabset
-rm -rf ${NEW_UCRT64_FOLDER}/share/pkgconfig
-rm -rf ${NEW_UCRT64_FOLDER}/share/bash-completion
-rm -rf ${NEW_UCRT64_FOLDER}/share/appdata
-rm -rf ${NEW_UCRT64_FOLDER}/share/gdb
-rm -rf ${NEW_UCRT64_FOLDER}/share/help
-rm -rf ${NEW_UCRT64_FOLDER}/share/gtk-doc
-rm -rf ${NEW_UCRT64_FOLDER}/share/doc
-rm -rf ${NEW_UCRT64_FOLDER}/share/applications
-rm -rf ${NEW_UCRT64_FOLDER}/share/devhelp
-rm -rf ${NEW_UCRT64_FOLDER}/share/gir-*
-rm -rf ${NEW_UCRT64_FOLDER}/share/graphite*
-rm -rf ${NEW_UCRT64_FOLDER}/share/installed-tests
-rm -rf ${NEW_UCRT64_FOLDER}/share/vala
-rm -f ${NEW_UCRT64_FOLDER}/share/sqlite/extensions/*.c
+rm -rf ${OUT_UCRT64_FOLDER}/var
+rm -rf ${OUT_UCRT64_FOLDER}/include
+rm -rf ${OUT_UCRT64_FOLDER}/libexec
+rm -rf ${OUT_UCRT64_FOLDER}/share/man
+rm -rf ${OUT_UCRT64_FOLDER}/share/readline
+rm -rf ${OUT_UCRT64_FOLDER}/share/info
+rm -rf ${OUT_UCRT64_FOLDER}/share/aclocal
+rm -rf ${OUT_UCRT64_FOLDER}/share/gnome-common
+rm -rf ${OUT_UCRT64_FOLDER}/share/glade
+rm -rf ${OUT_UCRT64_FOLDER}/share/gettext
+rm -rf ${OUT_UCRT64_FOLDER}/share/terminfo
+rm -rf ${OUT_UCRT64_FOLDER}/share/tabset
+rm -rf ${OUT_UCRT64_FOLDER}/share/pkgconfig
+rm -rf ${OUT_UCRT64_FOLDER}/share/bash-completion
+rm -rf ${OUT_UCRT64_FOLDER}/share/appdata
+rm -rf ${OUT_UCRT64_FOLDER}/share/gdb
+rm -rf ${OUT_UCRT64_FOLDER}/share/help
+rm -rf ${OUT_UCRT64_FOLDER}/share/gtk-doc
+rm -rf ${OUT_UCRT64_FOLDER}/share/doc
+rm -rf ${OUT_UCRT64_FOLDER}/share/applications
+rm -rf ${OUT_UCRT64_FOLDER}/share/devhelp
+rm -rf ${OUT_UCRT64_FOLDER}/share/gir-*
+rm -rf ${OUT_UCRT64_FOLDER}/share/graphite*
+rm -rf ${OUT_UCRT64_FOLDER}/share/installed-tests
+rm -rf ${OUT_UCRT64_FOLDER}/share/vala
+rm -f ${OUT_UCRT64_FOLDER}/share/sqlite/extensions/*.c
 # remove on the lib folder
-rm -rf ${NEW_UCRT64_FOLDER}/lib/atkmm*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/cairomm*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/cmake
-rm -rf ${NEW_UCRT64_FOLDER}/lib/dde*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/engines*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/gdkmm*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/gettext
-rm -rf ${NEW_UCRT64_FOLDER}/lib/giomm*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/girepository*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/glib*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/gtk*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/itcl*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/libxml*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/pango*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/python*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/pkgconfig
-rm -rf ${NEW_UCRT64_FOLDER}/lib/peas-demo
-rm -rf ${NEW_UCRT64_FOLDER}/lib/reg*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/sigc*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/sqlite*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/terminfo
-rm -rf ${NEW_UCRT64_FOLDER}/lib/tcl*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/tdbc*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/thread*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/tk*
-rm -rf ${NEW_UCRT64_FOLDER}/lib/*.sh
+rm -rf ${OUT_UCRT64_FOLDER}/lib/atkmm*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/cairomm*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/cmake
+rm -rf ${OUT_UCRT64_FOLDER}/lib/dde*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/engines*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/gdkmm*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/gettext
+rm -rf ${OUT_UCRT64_FOLDER}/lib/giomm*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/girepository*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/glib*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/gtk*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/itcl*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/libxml*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/pango*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/python*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/pkgconfig
+rm -rf ${OUT_UCRT64_FOLDER}/lib/peas-demo
+rm -rf ${OUT_UCRT64_FOLDER}/lib/reg*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/sigc*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/sqlite*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/terminfo
+rm -rf ${OUT_UCRT64_FOLDER}/lib/tcl*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/tdbc*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/thread*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/tk*
+rm -rf ${OUT_UCRT64_FOLDER}/lib/*.sh
 
 # remove the languages that we are not supporting
-LOCALE="${NEW_UCRT64_FOLDER}/share/locale"
+LOCALE="${OUT_UCRT64_FOLDER}/share/locale"
 LOCALE_TMP="${LOCALE}-tmp"
 mkdir ${LOCALE_TMP}
-for element_rel in $(ls ${GIT_CT_LANGUAGES_FOLDER})
+for element_rel in $(ls ${IN_CT_LANGUAGES_FOLDER})
 do
-  element_abs=${GIT_CT_LANGUAGES_FOLDER}/${element_rel}
+  element_abs=${IN_CT_LANGUAGES_FOLDER}/${element_rel}
   [ -d ${element_abs} ] && [ -d ${LOCALE}/${element_rel} ] && mv -fv ${LOCALE}/${element_rel} ${LOCALE_TMP}/
 done
 
@@ -221,52 +221,52 @@ rm -rf ${LOCALE}
 mv ${LOCALE_TMP} ${LOCALE}
 
 # strip the binaries to reduce the size
-find ${NEW_UCRT64_FOLDER} -name *.dll | xargs strip
-find ${NEW_UCRT64_FOLDER} -name *.exe | xargs strip
+find ${OUT_UCRT64_FOLDER} -name *.dll | xargs strip
+find ${OUT_UCRT64_FOLDER} -name *.exe | xargs strip
 
 
 echo "set use native windows theme..."
-[ -d ${NEW_ETC_GTK_FOLDER} ] || mkdir -p ${NEW_ETC_GTK_FOLDER}
-echo "[Settings]" > ${NEW_ETC_GTK_SETTINGS_INI}
-echo "gtk-theme-name=win32" >> ${NEW_ETC_GTK_SETTINGS_INI}
+[ -d ${OUT_ETC_GTK_FOLDER} ] || mkdir -p ${OUT_ETC_GTK_FOLDER}
+echo "[Settings]" > ${OUT_ETC_GTK_SETTINGS_INI}
+echo "gtk-theme-name=win32" >> ${OUT_ETC_GTK_SETTINGS_INI}
 
 
 echo "copying cherrytree files..."
 # exe
-strip ${GIT_CT_EXE}
-cp -v ${GIT_CT_EXE} ${NEW_UCRT64_FOLDER}/bin/
+strip ${IN_CT_EXE}
+cp -v ${IN_CT_EXE} ${OUT_UCRT64_FOLDER}/bin/
 # license
-cp -v ${GIT_CT_LICENSE} ${NEW_ROOT_FOLDER}/
+cp -v ${IN_CT_LICENSE} ${OUT_ROOT_FOLDER}/
 # share data
-mkdir -p ${NEW_CHERRYTREE_SHARE}/data
-cp -rv ${GIT_CT_LANGUAGE_SPECS_FOLDER} ${NEW_CHERRYTREE_SHARE}/
-cp -rv ${GIT_CT_STYLES_FOLDER} ${NEW_CHERRYTREE_SHARE}/
+mkdir -p ${OUT_CHERRYTREE_SHARE}/data
+cp -rv ${IN_CT_LANGUAGE_SPECS_FOLDER} ${OUT_CHERRYTREE_SHARE}/
+cp -rv ${IN_CT_STYLES_FOLDER} ${OUT_CHERRYTREE_SHARE}/
 for element_rel in script3.js \
                    styles4.css \
                    user-style.xml
 do
-  cp -v ${GIT_CT_DATA_FOLDER}/${element_rel} ${NEW_CHERRYTREE_SHARE}/data/
+  cp -v ${IN_CT_DATA_FOLDER}/${element_rel} ${OUT_CHERRYTREE_SHARE}/data/
 done
 # share icons
-mkdir -p ${NEW_CHERRYTREE_SHARE}/icons
-cp -v ${GIT_CT_ICONS_FOLDER}/ct_home.svg ${NEW_CHERRYTREE_SHARE}/icons/
-cp -r -v ${GIT_CT_ICONS_FOLDER}/Breeze_Dark_icons ${NEW_CHERRYTREE_SHARE}/icons/
-cp -r -v ${GIT_CT_ICONS_FOLDER}/Breeze_Light_icons ${NEW_CHERRYTREE_SHARE}/icons/
+mkdir -p ${OUT_CHERRYTREE_SHARE}/icons
+cp -v ${IN_CT_ICONS_FOLDER}/ct_home.svg ${OUT_CHERRYTREE_SHARE}/icons/
+cp -r -v ${IN_CT_ICONS_FOLDER}/Breeze_Dark_icons ${OUT_CHERRYTREE_SHARE}/icons/
+cp -r -v ${IN_CT_ICONS_FOLDER}/Breeze_Light_icons ${OUT_CHERRYTREE_SHARE}/icons/
 # i18n languages
-for element_rel in $(ls ${GIT_CT_LANGUAGES_FOLDER})
+for element_rel in $(ls ${IN_CT_LANGUAGES_FOLDER})
 do
-  element_abs=${GIT_CT_LANGUAGES_FOLDER}/${element_rel}
+  element_abs=${IN_CT_LANGUAGES_FOLDER}/${element_rel}
   [ -d ${element_abs} ] && cp -rfv ${element_abs} ${LOCALE}/
 done
 # spell check languages
-mkdir -p ${NEW_HUNSPELL_FOLDER}
-cp -v ${GIT_CT_HUNSPELL}/*.aff ${NEW_HUNSPELL_FOLDER}/
-cp -v ${GIT_CT_HUNSPELL}/*.dic ${NEW_HUNSPELL_FOLDER}/
-# fix issue with ucrt64/etc/ssl not installed properly in --root ${NEW_MSYS2_FOLDER}
-rm -rf ${NEW_UCRT64_FOLDER}/etc/ssl
-cp -rv ${OLD_UCRT64_FOLDER}/etc/ssl ${NEW_UCRT64_FOLDER}/etc/
+mkdir -p ${OUT_HUNSPELL_FOLDER}
+cp -v ${IN_CT_HUNSPELL}/*.aff ${OUT_HUNSPELL_FOLDER}/
+cp -v ${IN_CT_HUNSPELL}/*.dic ${OUT_HUNSPELL_FOLDER}/
+# fix issue with ucrt64/etc/ssl not installed properly in --root ${OUT_MSYS2_FOLDER}
+rm -rf ${OUT_UCRT64_FOLDER}/etc/ssl
+cp -rv ${OLD_UCRT64_FOLDER}/etc/ssl ${OUT_UCRT64_FOLDER}/etc/
 # nolatex folder
-cp -rv ${NEW_ROOT_FOLDER} ${NEW_ROOT_FOLDER_NOLATEX}
+cp -rv ${OUT_ROOT_FOLDER} ${OUT_ROOT_FOLDER_NOLATEX}
 # latex.exe
 for element_rel in latex.exe \
                    libkpathsea-6.dll \
@@ -274,10 +274,10 @@ for element_rel in latex.exe \
                    runscript.dll \
                    runscript.tlu
 do
-  cp -v ${OLD_UCRT64_FOLDER}/bin/${element_rel} ${NEW_UCRT64_FOLDER}/bin/
+  cp -v ${OLD_UCRT64_FOLDER}/bin/${element_rel} ${OUT_UCRT64_FOLDER}/bin/
 done
-cp -v ${OLD_UCRT64_FOLDER}/var/lib/texmf/web2c/pdftex/latex.fmt ${NEW_UCRT64_FOLDER}/bin/
-cp -rv ${OLD_UCRT64_FOLDER}/share/texmf-dist ${NEW_UCRT64_FOLDER}/share/
+cp -v ${OLD_UCRT64_FOLDER}/var/lib/texmf/web2c/pdftex/latex.fmt ${OUT_UCRT64_FOLDER}/bin/
+cp -rv ${OLD_UCRT64_FOLDER}/share/texmf-dist ${OUT_UCRT64_FOLDER}/share/
 # dvipng.exe
 for element_rel in dvipng.exe \
                    libgd.dll \
@@ -298,6 +298,9 @@ for element_rel in dvipng.exe \
                    libkvazaar-7.dll \
                    libcryptopp.dll
 do
-  cp -v ${OLD_UCRT64_FOLDER}/bin/${element_rel} ${NEW_UCRT64_FOLDER}/bin/
+  cp -v ${OLD_UCRT64_FOLDER}/bin/${element_rel} ${OUT_UCRT64_FOLDER}/bin/
 done
-cp -v ${OLD_UCRT64_FOLDER}/var/lib/texmf/fonts/map/dvips/updmap/ps2pk.map ${NEW_UCRT64_FOLDER}/bin/
+cp -v ${OLD_UCRT64_FOLDER}/var/lib/texmf/fonts/map/dvips/updmap/ps2pk.map ${OUT_UCRT64_FOLDER}/bin/
+
+7za a ${OUT_ROOT_FOLDER}.7z ${OUT_ROOT_FOLDER}
+7za a ${OUT_ROOT_FOLDER_NOLATEX}.7z ${OUT_ROOT_FOLDER_NOLATEX}
