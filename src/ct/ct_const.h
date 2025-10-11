0b211e36692b29b16aab4381a9d975a83f94e194
Giuseppe Penone <giuspen@gmail.com>
2025-10-11 09:11:57 +0100

    added icons for artificial intelligence copilot, claude

--- src/ct/ct_const.h
+++ src/ct/ct_const.h
@@ -546,6 +546,8 @@ const inline static std::vector<const gchar*> _NODE_CUSTOM_ICONS {
     "ct_locale",            // 258
     "ct_gemini",            // 259
     "ct_chatgpt",           // 260
+    "ct_copilot",           // 261
+    "ct_claude",            // 262
 };
 
 const inline static std::vector<int> NODE_CUSTOM_ICONS_ORDERED {
@@ -574,7 +576,7 @@ const inline static std::vector<int> NODE_CUSTOM_ICONS_ORDERED {
     133/*microchip*/, 164/*computer*/, 165/*display*/, 166/*drive hd*/, 167/*drive usb*/,
     /*coding start*/222/*git*/, 243/*stackoverflow*/, 181/*bug*/, 255/*spring-boot*/, 17, 257/*vim*/, 21, 22, 23, 24, 178, 180, 38, 46, 47, 48, 49, 140, 114,
     184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201/*coding end*/,
-    /*devops start*/134, 135, 136, 137, 138, 139, 182, 183, 245, 259/*gemini*/, 260/*chatgpt*/, 253/*devops end*/,
+    /*devops start*/134, 135, 136, 137, 138, 139, 182, 183, 245, 259/*gemini*/, 260/*chatgpt*/, 261/*copilot*/, 262/*claude*/, 253/*jenkins*/,
     /*weather start*/256, 147, 148, 149, 150, 151, 152, 153, 154, 155/*weather end*/,
     /*cherries start*/27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37/*cherries end*/,
     25/*bullet*/, 26/*noicon*/, 209/*bullseye*/, 220/*four_leaf_clover*/, 236/*pool_8_ball*/, 258/*locale*/,
