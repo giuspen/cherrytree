b37ad079dc30adf65c8dcd197205fffe626aa5fe
Giuseppe Penone <giuspen@gmail.com>
2026-07-18 13:41:05 +0100

    fixed regression text_row_cut(), text_row_copy(), text_row_delete() no longer including newline

--- src/ct/ct_actions_edit.cc
+++ src/ct/ct_actions_edit.cc
@@ -29,6 +29,18 @@
 #include "ct_storage_control.h"
 #include <gtkmm/dialog.h>
 
+namespace {
+
+CtTextRange include_trailing_newline_if_any(CtTextRange range)
+{
+    if (range.iter_end and range.iter_end.get_char() == '\n') {
+        range.iter_end.forward_char();
+    }
+    return range;
+}
+
+} // namespace (anonymous)
+
 // Step Back for the Current Node, if Possible
 void CtActions::requested_step_back()
 {
@@ -615,7 +627,8 @@ void CtActions::text_row_cut()
     if (not proof.text_view->get_buffer()) return;
     if (not _is_curr_node_not_read_only_or_error()) return;
 
-    CtTextRange range = CtList{_pCtConfig, proof.text_view->get_buffer()}.get_paragraph_iters();
+    CtTextRange range = include_trailing_newline_if_any(
+        CtList{_pCtConfig, proof.text_view->get_buffer()}.get_paragraph_iters());
     if (range.iter_start.get_offset() == range.iter_end.get_offset()) return;
     proof.text_view->get_buffer()->select_range(range.iter_start, range.iter_end);
     g_signal_emit_by_name(G_OBJECT(proof.text_view->gobj()), "cut-clipboard");
@@ -627,7 +640,8 @@ void CtActions::text_row_copy()
     auto proof = _get_text_view_n_buffer_codebox_proof();
     if (not proof.text_view->get_buffer()) return;
 
-    CtTextRange range = CtList{_pCtConfig, proof.text_view->get_buffer()}.get_paragraph_iters();
+    CtTextRange range = include_trailing_newline_if_any(
+        CtList{_pCtConfig, proof.text_view->get_buffer()}.get_paragraph_iters());
     if (range.iter_start.get_offset() == range.iter_end.get_offset()) return;
     proof.text_view->get_buffer()->select_range(range.iter_start, range.iter_end);
     g_signal_emit_by_name(G_OBJECT(proof.text_view->gobj()), "copy-clipboard");
@@ -640,7 +654,8 @@ void CtActions::text_row_delete()
     if (not proof.text_view->get_buffer()) return;
     if (not _is_curr_node_not_read_only_or_error()) return;
 
-    CtTextRange range = CtList{_pCtConfig, proof.text_view->get_buffer()}.get_paragraph_iters();
+    CtTextRange range = include_trailing_newline_if_any(
+        CtList{_pCtConfig, proof.text_view->get_buffer()}.get_paragraph_iters());
     if (range.iter_start.get_offset() == range.iter_end.get_offset()) return;
     proof.text_view->get_buffer()->erase(range.iter_start, range.iter_end);
     _pCtMainWin->get_state_machine().update_state();
